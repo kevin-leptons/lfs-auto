1d73d3b9a15305d1e9b7f3f2028e4e9a1a168a4d
kevin leptons <kevin.leptons@gmail.com>
2016-04-07 01:15:47 +0700

    add verify error for build check

--- script/build-temp-system/build-check.sh
+++ script/build-temp-system/build-check.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # using     : build check
+# time      : 0.1 sbu
+# params    : none
+# return    : 0 on successfull, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,25 +11,88 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# define variables
+package_name=check
+source_file=check-0.10.0.tar.gz
+source_dir=check-0.10.0
 
 # change working directory to sources directory
 cd $root_sources
 
-# extract source code and change to source code directory
-if [ ! -d check-0.10.0 ]; then
-   tar -xf check-0.10.0.tar.gz
-fi
-cd check-0.10.0
+# log start setup
+log_build "$package_name.setup.start" true
 
-# prepare
+# vefify
+if [ ! -f $source_file ]; then
+    log_build "$package_name.verify" false
+    exit 1
+else
+    log_build "$package_name.verify" true
+fi
+
+# extract source code and change to source code directory
+if [ ! -d $source_dir ]; then
+
+    log_build "$package_name.extract.start" true
+
+    tar -vxf $source_file
+
+    if [[ $? != 0 ]]; then
+        log_build "$package_name.extract.finish" false
+        exit 1
+    else
+        log_build "$package_name.extract.finish" true
+    fi
+else
+    log_build "$package_name.extract.idle" true
+fi
+cd $source_dir
+
+# configure
+log_build "$package_name.configure.start" true
 PKG_CONFIG= ./configure -- prefix=/tools
+if [[ $? != 0 ]]; then
+   log_build "$package_name.configure.finish" false
+   exit 1
+else
+   log_build "$package_name.configure.finish" true
+fi
 
 # build
+log_build "$package_name.make.start" true
 make
+if [[ $? != 0 ]]; then
+    log_build "$package_name.make.finish" false
+    exit 1
+else
+    log_build "$package_name.make.finish" true
+fi
 
 # test
+# test
+log_build "$package_name.test.start" true
 make check
+if [[ $? != 0 ]]; then
+    log_build "$package_name.test.finish" false
+    exit 1
+else
+    log_build "$package_name.test.finish" true
+fi
 
 # install
+log_build "$package_name.install.start" true
 make install
+if [[ $? != 0 ]]; then
+    log_build "$package_name.install.finish" false
+    exit 1
+else
+    log_build "$package_name.install.finish" true
+fi
+
+# successfull
+log_build "$package_name.setup.finish" true
+exit 0
