966b0dcad68d2fb61aab52c3097a2d25b587a738
kevin leptons <kevin.leptons@gmail.com>
2016-04-06 14:45:05 +0700

    add verify error for build glibc

--- script/build-temp-system/build-glibc.sh
+++ script/build-temp-system/build-glibc.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : build glibc package
+# params    : none
+# return    : 0 on successfull, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -13,21 +15,44 @@ source $script_dir/configuration.sh
 # change working directory to sources directory
 cd $root_sources
 
+# define variables
+source_file=glibc-2.22.tar.xz
+source_dir=glibc-2.22
+build_dir=glibc-build
+
+# verify source file
+if [ ! -f $source_file ]; then
+    echo "erorr: source file " $source_file " is not exist"
+    echo $?
+    exit 1
+fi
+
 # extract source code and change to source code directory
 if [ ! -d glibc-2.22 ]; then
    tar -xf glibc-2.22.tar.xz
+   if $?; then
+       echo "erorr: extract source file " $source_file
+       echo $?
+       exit 1
+   fi
 fi
-cd glibc-2.22 &&
+cd glibc-2.22
 
-# path package
-patch -Np1 -i ../glibc-2.22-upstream_i386_fix-1.patch &&
+# patch
+patch_file=../glibc-2.22-upstream_i386_fix-1.patch
+patch -Np1 -i $patch_file
+if $?; then
+    echo "erorr: patch source " $patch_file
+    echo $?
+    exit 1
+fi
 
 # change to source directory and create build directory
 cd ../
-mkdir -vp glibc-build
-cd glibc-build
+mkdir -vp $build_dir
+cd $build_dir
 
-# configure package
+# configure
 ../glibc-2.22/configure                             \
       --prefix=/tools                               \
       --host=$LFS_TGT                               \
@@ -39,12 +64,27 @@ cd glibc-build
       libc_cv_forced_unwind=yes                     \
       libc_cv_ctors_header=yes                      \
       libc_cv_c_cleanup=yes
+if $?; then
+    echo "erorr: configure " $source_file
+    echo $?
+    exit 1
+fi
 
 # build
 make
+if $?; then
+    echo "erorr: make " $source_file
+    echo $?
+    exit 1
+fi
 
 # install
 make install
+if $?; then
+    echo "erorr: install " $source_file
+    echo $?
+    exit 1
+fi
 
 # test
 # this is not way to get test result
