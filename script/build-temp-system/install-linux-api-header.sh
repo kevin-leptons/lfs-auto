001a2fb4baa13e869399abb64a3b06217da1cce9
kevin leptons <kevin.leptons@gmail.com>
2016-04-06 14:36:17 +0700

    add verify error for install linux api headers

--- script/build-temp-system/install-linux-api-header.sh
+++ script/build-temp-system/install-linux-api-header.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : install linux api header
+# params    : none
+# return    : 0 on successfull, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -13,15 +15,43 @@ source $script_dir/configuration.sh
 # change working directory to sources directory
 cd $root_sources
 
-# extract source code and change to source code directory
-if [ ! -d linux-4.2 ]; then
-   tar -xf linux-4.2.tar.xz
+# define variables
+source_file=linux-4.2.tar.xz
+source_dir=linux-4.2
+
+# verify source_file
+if [ ! -f $source_file]; then
+    echo "error: source file " $source_file " is not exists"
 fi
-cd linux-4.2 &&
+
+# extract source code and change to source code directory
+if [ ! -d $source_dir ]; then
+   tar -xf $source_file
+   if $?; then
+       echo "error: extract source file " $source_file
+       echo $?
+       exit 1
+   fi
+fi
+cd $source_dir
 
 # make sure there are no table files embedded in the packaged
 make mrproper
+if $?; then
+    echo "error: make mrproper " $source_file
+    echo $?
+    exit 1
+fi
 
-# install header
+# install
 make INSTALL_HDR_PATH=dest headers_install
+if $?; then
+    echo "error: install " $source_file
+    echo $?
+    exit 1
+fi
+
 cp -rv dest/include/* /tools/include
+
+# successfull
+exit 0
