be2f670d6a138422f250dc694678a82a4d217573
kevin leptons <kevin.leptons@gmail.com>
2016-04-06 15:51:41 +0700

    add verify error for build libstdc++

--- script/build-temp-system/build-libstdc++.sh
+++ script/build-temp-system/build-libstdc++.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : build libstdc++ package
+# params    : none
+# return    : 0 on successfull, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -13,16 +15,34 @@ source $script_dir/configuration.sh
 # change working directory to sources directory
 cd $root_sources
 
+# define variables
+source_file=gcc-5.2.0.tar.bz2
+source_dir=gcc-5.2.0
+build_dir=gcc-build
+
+# verify source file
+if [ ! -f $source_file ] then
+    echo "erorr: source file " $source_file " is not exist"
+    echo $?
+    exit 1
+fi
+
 # extract source code
-if [ ! -d gcc-5.2.0 ]; then
-    tar -xf gcc-5.2.0.tar.bz2
+if [ ! -d $source_dir ]; then
+    tar -xf $source_file
+    if $?; then
+        echo "erorr: extract source file " $source_file
+        echo $?
+        exit 1
+    fi
 fi
 
 # create and change to build directory
-rm -rf gcc-build &&
-mkdir -vp gcc-build &&
-cd gcc-build &&
+rm -rf $build_dir
+mkdir -vp $build_dir
+cd $build_dir
 
+# configure
 ../gcc-5.2.0/libstdc++-v3/configure \
     --host=$LFS_TGT                 \
     --prefix=/tools                 \
@@ -30,10 +50,28 @@ cd gcc-build &&
     --disable-nls                   \
     --disable-libstdcxx-threads     \
     --disable-libstdcxx-pch         \
-    --with-gxx-include-dir=/tools/$LFS_TGT/include/c++/5.2.0 &&
+    --with-gxx-include-dir=/tools/$LFS_TGT/include/c++/5.2.0
+if $?; then
+    echo "erorr: configure " $source_file
+    echo $?
+    exit 1
+fi
 
 # build
-make &&
+make
+if $?; then
+    echo "erorr: make " $source_file
+    echo $?
+    exit 1
+fi
 
 # install
 make install
+if $?; then
+    echo "erorr: make install " $source_file
+    echo $?
+    exit 1
+fi
+
+# successfull
+exit 0
