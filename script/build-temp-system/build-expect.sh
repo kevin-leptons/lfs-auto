f11d89b03a45220db88aa3b02b90ee0ec671ecde
kevin leptons <kevin.leptons@gmail.com>
2016-04-07 00:38:53 +0700

    add verify error for build tcl-core and expect

--- script/build-temp-system/build-expect.sh
+++ script/build-temp-system/build-expect.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # using     : build expect
+# time      : 0.1 sbu
+# params    : none
+# return    : 0 on successfull, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,31 +11,99 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# define variables
+package_name=expect
+source_file=expect5.45.tar.gz
+source_dir=expect5.45
 
 # change working directory to sources directory
 cd $root_sources
 
-# extract source code and change to source code directory
-if [ ! -d expect5.45 ]; then
-   tar -xf expect5.45.tar.gz 
+# log start setup
+log_build "$package_name.setup.start" true
+
+# vefify
+if [ ! -f $source_file ]; then
+    log_build "$package_name.verify" false
+    exit 1
+else
+    log_build "$package_name.verify" true
 fi
-cd expect5.45
+
+# extract source code and change to source code directory
+if [ ! -d $source_dir ]; then
+
+    log_build "$package_name.extract.start" true
+
+    tar -vxf $source_file
+
+    if [[ $? != 0 ]]; then
+        log_build "$package_name.extract.finish" false
+        exit 1
+    else
+        log_build "$package_name.extract.finish" true
+    fi
+else
+    log_build "$package_name.extract.idle" true
+fi
+cd $source_dir
 
 # fix configure file
 cp -v configure{,.orig} &&
-sed 's:/usr/local/bin:/bin:' configure.orig > configure &&
+sed 's:/usr/local/bin:/bin:' configure.orig > configure
+if [[ $? != 0 ]]; then
+    log_build "$package_name.fix configure file" false
+    exit 1
+else
+    log_build "$package_name.fix configure file" true
+fi
 
 # configure
+log_build "$package_name.configure.start" true
 ./configure --prefix=/tools       \
    --with-tcl=/tools/lib \
-   --with-tclinclude=/tools/include &&
+   --with-tclinclude=/tools/include
+if [[ $? != 0 ]]; then
+   log_build "$package_name.configure.finish" false
+   exit 1
+else
+   log_build "$package_name.configure.finish" true
+fi
 
 # build
-make &&
+log_build "$package_name.make.start" true
+make
+if [[ $? != 0 ]]; then
+    log_build "$package_name.make.finish" false
+    exit 1
+else
+    log_build "$package_name.make.finish" true
+fi
 
 # test
-make test &&
+log_build "$package_name.test.start" true
+make test
+if [[ $? != 0 ]]; then
+    log_build "$package_name.test.finish" false
+    exit 1
+else
+    log_build "$package_name.test.finish" true
+fi
 
 # install
+log_build "$package_name.install.start" true
 make SCRIPTS="" install
+if [[ $? != 0 ]]; then
+    log_build "$package_name.install.finish" false
+    exit 1
+else
+    log_build "$package_name.install.finish" true
+fi
+
+# successfull
+log_build "$package_name.setup.finish" true
+exit 0
--- script/build-temp-system/build-tcl-core.sh
+++ script/build-temp-system/build-tcl-core.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # using     : build tcl-core
+# time      : 0.5 sbu
+# params    : none
+# return    : 0 on successfull, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,35 +11,103 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# define variables
+package_name=tcl-core
+source_file=tcl-core8.6.4-src.tar.gz
+source_dir=tcl8.6.4
 
 # change working directory to sources directory
 cd $root_sources
 
-# extract source code and change to source directory
-if [ ! -d tcl8.6.4 ]; then
-   tar -xf tcl-core8.6.4-src.tar.gz
-fi
-cd tcl8.6.4
+# log start setup
+log_build "$package_name.setup.start" true
 
-# prepare
+# vefify
+if [ ! -f $source_file ]; then
+    log_build "$package_name.verify" false
+else
+    log_build "$package_name.verify" true
+fi
+
+# extract source code and change to source directory
+if [ ! -d $source_dir ]; then
+   tar -xf $source_file
+fi
+cd $source_dir
+
+# configure
+log_build "$package_name.configure.start" true
 cd unix
 ./configure --prefix=/tools
+if [[ $? != 0 ]]; then
+    log_build "$package_name.configure.finish" false
+    exit 1
+else
+    log_build "$package_name.configure.finish" true
+fi
 
 # build
+log_build "$package_name.make.start" true
 make
+if [[ $? != 0 ]]; then
+    log_build "$package_name.make.finish" false
+    exit 1
+else
+    log_build "$package_name.make.finish" true
+fi
 
 # test
+log_build "$package_name.test.start" true
 TZ=UTC make test
+if [[ $? != 0 ]]; then
+    log_build "$package_name.test.finish" false
+    exit 1
+else
+    log_build "$package_name.test.finish" true
+fi
 
 # install
+log_build "$package_name.install.start" true
 make install
+if [[ $? != 0 ]]; then
+    log_build "$package_name.install.finish" false
+    exit 1
+else
+    log_build "$package_name.install.finish" true
+fi
 
 # make library writeable, so can removed later
 chmod -v u+x /tools/lib/libtcl8.6.so
+if [[ $? != 0 ]]; then
+    log_build "$package_name.make library writeable" false
+    exit 1
+else
+    log_build "$package_name.make library writeable" true
+fi
 
 # install tcl headers
+log_build "$package_name.install tcl headers.start" true
 make install-private-headers
+if [[ $? != 0 ]]; then
+    log_build "$package_name.install tcl headers.finish" false
+    exit 1
+else
+    log_build "$package_name.install tcl headers.finish" true
+fi
 
 # link
 ln -sv tclsh8.6 /tools/bin/tclsh
+if [[ $? != 0 ]]; then
+    log_build "$package_name.link.finish" false
+    exit 1
+else
+    log_build "$package_name.link.finish" true
+fi
+
+# successfull
+log_build "$package_name.setup.finish" true
+exit 0
