9c5d8a722616360da8cca391a7e489f8f1f46034
kevin leptons <kevin.leptons@gmail.com>
2016-04-06 23:34:06 +0700

    fix verify error for build glibc

--- script/build-temp-system/build-glibc.sh
+++ script/build-temp-system/build-glibc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # using     : build glibc package
+# time      : 4.5 sbu
 # params    : none
 # return    : 0 on successfull, 1 on error
 # author    : kevin.leptons@gmail.com
@@ -10,41 +11,56 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
 
 # change working directory to sources directory
 cd $root_sources
 
 # define variables
+package_name=glibc
 source_file=glibc-2.22.tar.xz
 source_dir=glibc-2.22
 build_dir=glibc-build
 
+# log start setup
+log_build "$package_name.setup.start" true
+
 # verify source file
 if [ ! -f $source_file ]; then
-    echo "erorr: source file " $source_file " is not exist"
-    echo $?
+    log_build "$package_name.vefify" false
     exit 1
+else
+    log_build "$package_name.vefify" true
 fi
 
 # extract source code and change to source code directory
-if [ ! -d glibc-2.22 ]; then
-   tar -xf glibc-2.22.tar.xz
-   if $?; then
-       echo "erorr: extract source file " $source_file
-       echo $?
-       exit 1
-   fi
+if [ ! -d $source_dir ]; then
+
+    log_build "$package_name.extract.start" true
+
+    tar -vxf $source_file
+
+    if [[ $? != 0 ]]; then
+        log_build "$package_name.extract.finish" false
+        exit 1
+    else
+        log_build "$package_name.extract.finish" true
+    fi
+else
+    log_build "$package_name.extract.idle" true
 fi
-cd glibc-2.22
+cd $source_dir
 
 # patch
 patch_file=../glibc-2.22-upstream_i386_fix-1.patch
 patch -Np1 -i $patch_file
-if $?; then
-    echo "erorr: patch source " $patch_file
-    echo $?
+if [[ $? != 0 ]]; then
+    log_build "$package_name.patch" false
     exit 1
+else
+    log_build "$package_name.patch" true
 fi
 
 # change to source directory and create build directory
@@ -53,6 +69,7 @@ mkdir -vp $build_dir
 cd $build_dir
 
 # configure
+log_build "$package_name.configure.start" true
 ../glibc-2.22/configure                             \
       --prefix=/tools                               \
       --host=$LFS_TGT                               \
@@ -64,26 +81,31 @@ cd $build_dir
       libc_cv_forced_unwind=yes                     \
       libc_cv_ctors_header=yes                      \
       libc_cv_c_cleanup=yes
-if $?; then
-    echo "erorr: configure " $source_file
-    echo $?
+if [[ $? != 0 ]]; then
+    log_build "$package_name.configure.finish" false
     exit 1
+else
+    log_build "$package_name.configure.finish" true
 fi
 
 # build
+log_build "$package_name.make.start" true
 make
-if $?; then
-    echo "erorr: make " $source_file
-    echo $?
+if [[ $? != 0 ]]; then
+    log_build "$package_name.make.finish" false
     exit 1
+else
+    log_build "$package_name.make.finish" true
 fi
 
 # install
+log_build "$package_name.install.start" true
 make install
-if $?; then
-    echo "erorr: install " $source_file
-    echo $?
+if [[ $? != 0 ]]; then
+    log_build "$package_name.install.finish" false
     exit 1
+else
+    log_build "$package_name.install.finish" true
 fi
 
 # test
@@ -93,3 +115,7 @@ echo 'int main() {}' > dummy.c
 $LFS_TGT-gcc dummy.c
 readelf -l a.out | grep '\: /tools'
 rm -v dummy.c a.out
+
+# successfull
+log_build "$package_name.setup.finish" true
+exit 0
