a5d7d4d01ef1715461f1b29bd9e999c78adb0122
kevin leptons <kevin.leptons@gmail.com>
2016-04-07 13:13:14 +0700

    fix build time for build texinfo

--- script/build-temp-system/build-texinfo.sh
+++ script/build-temp-system/build-texinfo.sh
@@ -11,25 +11,87 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# define variables
+package_name=texinfo
+source_file=texinfo-6.0.tar.xz
+source_dir=texinfo-6.0
 
 # change working directory to sources directory
 cd $root_sources
 
-# extract source code and change to source code directory
-if [ ! -d texinfo-6.0 ]; then
-   tar -xf texinfo-6.0.tar.xz
+# log start setup
+log_build "$package_name.setup.start" true
+
+# verify source code
+if [ ! -f $source_file ]; then
+    log_build "$package_name.verify" false
+    exit 1
+else
+    log_build "$package_name.verify" true
 fi
-cd texinfo-6.0
+
+# extract source code and change to source code directory
+if [ ! -d $source_dir ]; then
+
+    log_build "$package_name.extract.start" true
+
+    tar -vxf $source_file
+
+    if [[ $? != 0 ]]; then
+        log_build "$package_name.extract.finish" false
+        exit 1
+    else
+        log_build "$package_name.extract.finish" true
+    fi
+else
+    log_build "$package_name.extract.idle" true
+fi
+cd $source_dir
 
 # configure
-./configure --prefix=/tools &&
+log_build "$package_name.configure.start" true
+./configure --prefix=/tools
+if [[ $? != 0 ]]; then
+    log_build "$package_name.configure.finish" false
+    exit 1
+else
+    log_build "$package_name.configure.finish" true
+fi
 
-# make
-make &&
+# build
+log_build "$package_name.make.start" true
+make
+if [[ $? != 0 ]]; then
+    log_build "$package_name.make.finish" false
+    exit 1
+else
+    log_build "$package_name.make.finish" true
+fi
 
 # test
+log_build "$package_name.test.start" true
 make check
+if [[ $? != 0 ]]; then
+    log_build "$package_name.test.finish" false
+    exit 1
+else
+    log_build "$package_name.test.finish" true
+fi
 
 # install
+log_build "$package_name.install.start" true
 make install
+if [[ $? != 0 ]]; then
+    log_build "$package_name.install.finish" false
+    exit 1
+else
+    log_build "$package_name.install.finish" true
+fi
+
+# successfull
+log_build "$package_name.setup.finish" true
+exit 0
