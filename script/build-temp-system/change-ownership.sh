b7c3b5c489ff643c2ab0fbfdd94df3a9a8276741
kevin leptons <kevin.leptons@gmail.com>
2016-04-07 14:08:46 +0700

    add verify error for change ownership

--- script/build-temp-system/change-ownership.sh
+++ script/build-temp-system/change-ownership.sh
@@ -8,7 +8,17 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# define variables
+task_name="change ownership"
 
 # change ownership
 sudo chown -R root:root $LFS/tools
+if [[ $? != 0 ]]; then
+    log_build "$task_name" false
+else
+    log_build "$task_name" true
+fi
