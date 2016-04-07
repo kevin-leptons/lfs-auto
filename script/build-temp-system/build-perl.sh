d6e04e736108a653b6ddfc2ab7e598970abea4b9
kevin leptons <kevin.leptons@gmail.com>
2016-04-07 12:56:07 +0700

    add verify error for build perl

--- script/build-temp-system/build-perl.sh
+++ script/build-temp-system/build-perl.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # using     : build perl
+# time      : 1.4 sbu
+# params    : none
+# return    : 0 on successfull, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,24 +11,78 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# define variables
+package_name=perl
+source_file=perl-5.22.0.tar.bz2
+source_dir=perl-5.22.0
 
 # change working directory to sources directory
 cd $root_sources
 
-# extract source code and change to source code directory
-if [ ! -d perl-5.22.0 ]; then
-   tar -xf perl-5.22.0.tar.bz2
-fi
-cd perl-5.22.0
+# log start setup
+log_build "$package_name.setup.start" true
 
-# prepare
-sh Configure -des -Dprefix=/tools -Dlibs=-lm &&
+# verify source code
+if [ ! -f $source_file ]; then
+    log_build "$package_name.verify" false
+    exit 1
+else
+    log_build "$package_name.verify" true
+fi
+
+# extract source code and change to source code directory
+if [ ! -d $source_dir ]; then
+
+    log_build "$package_name.extract.start" true
+
+    tar -vxf $source_file
+
+    if [[ $? != 0 ]]; then
+        log_build "$package_name.extract.finish" false
+        exit 1
+    else
+        log_build "$package_name.extract.finish" true
+    fi
+else
+    log_build "$package_name.extract.idle" true
+fi
+cd $source_dir
+
+# configure
+log_build "$package_name.configure.start" true
+sh Configure -des -Dprefix=/tools -Dlibs=-lm
+if [[ $? != 0 ]]; then
+    log_build "$package_name.configure.finish" false
+    exit 1
+else
+    log_build "$package_name.configure.finish" true
+fi
 
 # build
-make &&
+log_build "$package_name.make.start" true
+make
+if [[ $? != 0 ]]; then
+    log_build "$package_name.make.finish" false
+    exit 1
+else
+    log_build "$package_name.make.finish" true
+fi
 
 # install
 cp -v perl cpan/podlators/pod2man /tools/bin &&
-mkdir -pv /tools/lib/perl5/5.22.0 && 
+mkdir -pv /tools/lib/perl5/5.22.0 &&
 cp -Rv lib/* /tools/lib/perl5/5.22.0
+if [[ $? != 0 ]]; then
+    log_build "$package_name.install.finish" false
+    exit 1
+else
+    log_build "$package_name.install.finish" true
+fi
+
+# successfull
+log_build "$package_name.setup.finish" true
+exit 0
