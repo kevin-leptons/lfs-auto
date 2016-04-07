25c9946b7f9356f8a015990b871c2b5198c6575b
kevin leptons <kevin.leptons@gmail.com>
2016-04-07 11:38:46 +0700

    add verify error for build findutils

--- script/build-temp-system/build-findutils.sh
+++ script/build-temp-system/build-findutils.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# using     : build gcc package in pass 1
+# using     : build findutils
+# time      : 0.2 sbu
+# params    : none
+# return    : 0 on successfull, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,22 +11,77 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# define variables
+package_name=findutils
+source_file=findutils-4.4.2.tar.gz
+source_dir=findutils-4.4.2
 
 # change working directory to sources directory
 cd $root_sources
 
-# extract source code and change to source code directory
-if [ ! -d findutils-4.4.2 ]; then
-   tar -xf findutils-4.4.2.tar.gz
+# log start setup
+log_build "$package_name.setup.start" true
+
+# verify source code
+if [ ! -f $source_file ]; then
+    log_build "$package_name.verify" false
+    exit 1
+else
+    log_build "$package_name.verify" true
 fi
-cd findutils-4.4.2
+
+# extract source code and change to source code directory
+if [ ! -d $source_dir ]; then
+
+    log_build "$package_name.extract.start" true
+
+    tar -vxf $source_file
+
+    if [[ $? != 0 ]]; then
+        log_build "$package_name.extract.finish" false
+        exit 1
+    else
+        log_build "$package_name.extract.finish" true
+    fi
+else
+    log_build "$package_name.extract.idle" true
+fi
+cd $source_dir
 
 # configure
-./configure --prefix=/tools &&
+log_build "$package_name.configure.start" true
+./configure --prefix=/tools
+if [[ $? != 0 ]]; then
+    log_build "$package_name.configure.finish" false
+    exit 1
+else
+    log_build "$package_name.configure.finish" true
+fi
 
 # test
-make check &&
+log_build "$package_name.test.start" true
+make check
+if [[ $? != 0 ]]; then
+    log_build "$package_name.test.finish" false
+    exit 1
+else
+    log_build "$package_name.test.finish" true
+fi
 
 # install
+log_build "$package_name.install.start" true
 make install
+if [[ $? != 0 ]]; then
+    log_build "$package_name.install.finish" false
+    exit 1
+else
+    log_build "$package_name.install.finish" true
+fi
+
+# successfull
+log_build "$package_name.setup.finish" true
+exit 0
