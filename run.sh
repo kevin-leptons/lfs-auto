d20d8ac5702ed716d29eda1b1e67faccf91a267a
kevin leptons <kevin.leptons@gmail.com>
2016-04-08 10:11:05 +0700

    add verify error for run.sh

--- run.sh
+++ run.sh
@@ -8,13 +8,36 @@ __dir__="$(dirname "$0")"
 script_dir="$(readlink -f $__dir__/script)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $__dir__/util.sh
+
+# define variables
+task_name="lfs-auto"
+
+# log start run
+log "$task_name.start" true
+
+# prepare packages
+./prepare-package.sh
+if [[ $? != 0 ]]; then
+    log "$task_name.finish" false
+    exit 1
+fi
 
 # prepare virtual disk
-./prepare-disk.sh &&
+./prepare-disk.sh
+if [[ $? != 0 ]]; then
+    log "$task_name.finish" false
+    exit 1
+fi
 
 # build docker
-docker build -t $docker_name $__dir__ &&
+docker build -t $docker_name $__dir__
+if [[ $? != 0 ]]; then
+    log "$task_name.finish" false
+    exit 1
+fi
 
 # run docker
 # mount hard disk use to build lfs into docker
@@ -22,3 +45,11 @@ docker build -t $docker_name $__dir__ &&
 lfs_disk_path=$(realpath $__dir__/disk/$lfs_disk_file)
 docker run -ti --privileged -v $root:$root -v \
    $script_dir:$docker_script_dir $docker_name
+if [[ $? != 0 ]]; then
+   log "$task_name.finish" false
+   exit 1
+fi
+
+# successfull
+log "$task_name.finish" true
+exit 0
