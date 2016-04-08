c996b9fe2a82833d5c07bd87286ae007057d44f9
kevin leptons <kevin.leptons@gmail.com>
2016-04-08 12:06:27 +0700

    add verify error to create build user

--- script/create-build-user.sh
+++ script/create-build-user.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : create user use to build lfs
+# params    : none
+# return    : 0 on successfull, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -10,19 +12,62 @@ __dir__="$(dirname "$0")"
 source $__dir__/require-root.sh
 
 # use configuration
+# use util
 source $__dir__/configuration.sh
+source $__dir__/util.sh
+
+# define variables
+task_name="create-build-user"
+
+# log start
+log "$task_name.start"
 
 # create user group
 groupadd $build_user_group
+if [[ $? != 0 ]]; then
+    log "build-group.create" false
+    exit 1
+else
+    log "build-group.create" true
+fi
 
 # create user
 useradd -s /bin/bash -g $build_user_group -m -k /dev/null $build_user
+if [[ $? != 0 ]]; then
+    log "build-user.create" false
+    exit 1
+else
+    log "build-user.create" true
+fi
 
 # change user password
 echo -e "$build_user_password\n$build_user_password" | passwd $build_user
+if [[ $? != 0 ]]; then
+    log "build-user.password.change" false
+    exit 1
+else
+    log "build-user.password.change" true
+fi
 
 # create profile
 cp -v $__dir__/asset/build-user-profile.sh $build_user_home/.profile
+echo -e "$build_user_password\n$build_user_password" | passwd $build_user
+if [[ $? != 0 ]]; then
+    log "build-user.profile.create" false
+    exit 1
+else
+    log "build-user.profile.create" true
+fi
 
 # create bashrc
 cp -v $__dir__/asset/build-user-bashrc.sh $build_user_home/.bashrc
+echo -e "$build_user_password\n$build_user_password" | passwd $build_user
+if [[ $? != 0 ]]; then
+    log "build-user.bashrc.create" false
+    exit 1
+else
+    log "build-user.bashrc.create" true
+fi
+
+# successfull
+exit 0
