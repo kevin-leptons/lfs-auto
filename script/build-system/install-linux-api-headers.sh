3d17e4d2c67e783847f77a1631671dc94e8c4a3d
kevin leptons <kevin.leptons@gmail.com>
2016-04-11 00:38:07 +0700

    add verify error for install-linux-api-header

--- script/build-system/install-linux-api-headers.sh
+++ script/build-system/install-linux-api-headers.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : install linux api headers
+# params    : none
+# return    : 0 on successfull, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,21 +10,59 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# define variables
+package_name="linux"
+source_file=linux-4.2.tar.xz
+source_dir=linux-4.2
 
 # change working directory to sources directory
 cd /sources
 
+# log start
+log "$package_name.setup.start" true
+
 # extract source code and change to source directory
-if [ ! -d linux-4.2 ]; then
-   tar -xf linux-4.2.tar.xz
+if [ ! -d $source_dir ]; then
+
+    log "$package_name.extract.start" true
+
+    tar -vxf $source_file
+
+    if [[ $? != 0 ]]; then
+        log "$package_name.extract.finish" false
+        exit 1
+    else
+        log "$package_name.extract.finish" true
+    fi
+else
+    log "$package_name.extract.idle" true
 fi
-cd linux-4.2
+cd $source_dir
 
 # make sure there are no stale files and dependencies
 make mrproper
+if [[ $? != 0 ]]; then
+    log "$package_name.mrproper" false
+    exit 1
+else
+    log "$package_name.mrproper" true
+fi
 
 # install
 make INSTALL_HDR_PATH=dest headers_install &&
 find dest/include \( -name .install -o -name ..install.cmd \) -delete &&
 cp -rv dest/include/* /usr/include
+if [[ $? != 0 ]]; then
+    log "$package_name.install" false
+    exit 1
+else
+    log "$package_name.install" true
+fi
+
+# successfully
+log "$package_name.setup.finish" true
+exit 0
