54485e9d0991c1485039ca4316048d990051795f
kevin leptons <kevin.leptons@gmail.com>
2016-04-10 16:26:28 +0700

    Update prepare-package.sh

--- prepare-package.sh
+++ prepare-package.sh
@@ -26,7 +26,7 @@ log "$task_name.start" true
 sudo apt-get install apt-transport-https ca-certificates
 sudo  apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 \
     --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-cat /etc/apt/sources.list.d/docker.list << EOF \
+cat > /etc/apt/sources.list.d/docker.list << "EOF" \
 deb https://apt.dockerproject.org/repo debian-jessie main \
 EOF
 apt-cache policy docker-engine
