801e31218080c3fea9ada87265c34a7505c545cb
kevin leptons <kevin.leptons@gmail.com>
2016-04-11 22:58:51 +0700

    add verify error for build kmod

--- script/build-system/build-kmod.sh
+++ script/build-system/build-kmod.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : build kmod
+# params    : none
+# return    : 0 on success, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,33 +10,65 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# variables
+package_name="kmod"
+source_file="kmod-21.tar.xz"
+source_dir="kmod-21"
+
+# log start
+log_auto "$package_name.setup.start" 0
 
 # change working directory to sources directory
-cd /sources &&
+cd /sources
+
+# verify
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
+else
+    log_auto "$package_name.verify" 1
+fi
 
 # extract source code and change to source directory
-if [ ! -d kmod-21 ]; then
-   tar -xf kmod-21.tar.xz
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
+else
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.finish" $?
 fi
-cd kmod-21
+cd $source_dir
 
 # configure
+log_auto "$package_name.configure.start" 0
 ./configure --prefix=/usr          \
    --bindir=/bin          \
    --sysconfdir=/etc      \
    --with-rootlibdir=/lib \
    --with-xz              \
-   --with-zlib &&
+   --with-zlib
+log_auto "$package_name.configure.finish" $?
 
 # build
-make &&
+log_auto "$package_name.make.start" 0
+make
+log_auto "$package_name.make.finish" $?
 
 # install
-make install &&
+log_auto "$package_name.install.start" 0
+make install
+log_auto "$package_name.install.finish" $?
 
+# link
 for target in depmod insmod lsmod modinfo modprobe rmmod; do
    ln -sv ../bin/kmod /sbin/$target
 done
-
 ln -sv kmod /bin/lsmod
+log_auto "$package_name.link" $?
+
+# successfully
+log_auto "$package_name.setup.finish" $?
+exit 0
