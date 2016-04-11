fc513d632a4a172c4726fdeae6a45b3ff33a58c0
kevin leptons <kevin.leptons@gmail.com>
2016-04-11 23:33:21 +0700

    add verify error for build man-db

--- script/build-system/build-man-db.sh
+++ script/build-system/build-man-db.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : build man-db
+# params    : none
+# return    : 0 on success, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,33 +10,64 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# variables
+package_name="man-db"
+source_file="man-db-2.7.2.tar.xz"
+source_dir="man-db-2.7.2"
+
+# log start
+log_auto "$package_name.setup.start" 0
 
 # change working directory to sources directory
-cd /sources &&
+cd /sources
+
+# verify
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
+else
+    log_auto "$package_name.verify" 1
+fi
 
 # extract source code and change to source directory
-if [ ! -d man-db-2.7.2 ]; then
-   tar -xf man-db-2.7.2.tar.xz
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
+else
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.finish" $?
 fi
-cd man-db-2.7.2
+cd $source_dir
 
 # configure
+log_auto "$package_name.configure.start" 0
 ./configure --prefix=/usr                        \
    --docdir=/usr/share/doc/man-db-2.7.2 \
    --sysconfdir=/etc                    \
    --disable-setuid                     \
    --with-browser=/usr/bin/lynx         \
    --with-vgrind=/usr/bin/vgrind        \
-   --with-grap=/usr/bin/grap &&
+   --with-grap=/usr/bin/grap
+log_auto "$package_name.configure.finish" $?
 
 # build
-make &&
+log_auto "$package_name.make.start" 0
+make
+log_auto "$package_name.make.finish" $?
 
 # test
-make check &&
+log_auto "$package_name.test.start" 0
+make check
+log_auto "$package_name.test.finish" $?
 
 # install
+log_auto "$package_name.install.start" 0
 make install
+log_auto "$package_name.install.finish" $?
 
-
+# successfully
+log_auto "$package_name.setup.finish" $?
+exit 0
