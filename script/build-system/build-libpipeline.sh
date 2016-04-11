87892f8f5496abf139d7f86b4f71cc5d8e1aa8be
kevin leptons <kevin.leptons@gmail.com>
2016-04-11 23:00:49 +0700

    add verify error for build libpipeline

--- script/build-system/build-libpipeline.sh
+++ script/build-system/build-libpipeline.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : build libpipeline
+# params    : none
+# return    : 0 on success, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,25 +10,58 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# variables
+package_name="libpipeline"
+source_file="libpipeline-1.4.1.tar.gz"
+source_dir="libpipeline-1.4.1"
+
+# log start
+log_auto "$package_name.setup.start" 0
 
 # change working directory to sources directory
-cd /sources &&
+cd /sources
+
+# verify
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
+else
+    log_auto "$package_name.verify" 1
+fi
 
 # extract source code and change to source directory
-if [ ! -d libpipeline-1.4.1 ]; then
-   tar -xf libpipeline-1.4.1.tar.gz
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
+else
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.finish" $?
 fi
-cd libpipeline-1.4.1
+cd $source_dir
 
 # configure
-PKG_CONFIG_PATH=/tools/lib/pkgconfig ./configure --prefix=/usr &&
+log_auto "$package_name.configure.start" 0
+PKG_CONFIG_PATH=/tools/lib/pkgconfig ./configure --prefix=/usr
+log_auto "$package_name.configure.finish" $?
 
 # build
-make &&
+log_auto "$package_name.make.start" 0
+make
+log_auto "$package_name.make.finish" $?
 
 # test
-make check &&
+log_auto "$package_name.test.start" 0
+make check
+log_auto "$package_name.test.finish" $?
 
 # install
+log_auto "$package_name.install.start" 0
 make install
+log_auto "$package_name.install.finish" $?
+
+# successfully
+log_auto "$package_name.setup.finish" $?
+exit 0
