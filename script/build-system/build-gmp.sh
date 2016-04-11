15e284233f5d433a9586d39295919dc2f117c7f7
kevin leptons <kevin.leptons@gmail.com>
2016-04-11 15:25:21 +0700

    fix verify error for build gmp

--- script/build-system/build-gmp.sh
+++ script/build-system/build-gmp.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : build gmp
+# params    : none
+# return    : 0 on success, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,32 +10,64 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# define variables
+package_name="gmp"
+source_file="gmp-6.0.0a.tar.xz"
+source_dir="gmp-6.0.0a"
+
+# log start
+log_auto "$package_name.setup.start" 0
 
 # change working directory to sources directory
-cd /sources &&
+cd /sources
+
+# verify
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
+else
+    log_auto "$package_name.verify" 1
+fi
 
 # extract source code and change to source directory
-if [ ! -d gmp-6.0.0a ]; then
-   tar -xf gmp-6.0.0a.tar.xz
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
+else
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.start" $?
 fi
-cd gmp-6.0.0
+cd $source_dir
 
 # configure
+log_auto "$package_name.configure.start" 0
 ./configure --prefix=/usr    \
    --enable-cxx     \
    --disable-static \
-   --docdir=/usr/share/doc/gmp-6.0.0a &&
-
+   --docdir=/usr/share/doc/gmp-6.0.0a
+log_auto "$package_name.configure.finish" $?
 
 # build
+log_auto "$package_name.make.start" 0
 make &&
-make html &&
+make html
+log_auto "$package_name.make.finish" $?
 
 # test
+log_auto "$package_name.test.start" 0
 make check 2>&1 | tee gmp-check-log &&
 awk '/tests passed/{total+=$2} ; END{print total}' gmp-check-log
+log_auto "$package_name.test.finish" $?
 
 # install
+log_auto "$package_name.install.start" 0
 make install &&
 make install-html
+log_auto "$package_name.install.finish" $?
+
+# successfully
+log_auto "$package_name.setup.finish" $?
+exit 0
