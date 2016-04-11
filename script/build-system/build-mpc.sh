9e890fb283132de11f6c71c53bc797290dba3b96
kevin leptons <kevin.leptons@gmail.com>
2016-04-11 15:31:20 +0700

    fix verify error for build mpc

--- script/build-system/build-mpc.sh
+++ script/build-system/build-mpc.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : build mpc
+# params    : none
+# return    : 0 on success, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,29 +10,62 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# define variables
+package_name="mpc"
+source_file="mpc-1.0.3.tar.gz"
+source_dir="mpc-1.0.3"
+
+# log start
+log_auto "$package_name.setup.start" 0
 
 # change working directory to sources directory
-cd /sources &&
+cd /sources
+
+# verify
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
+else
+    log_auto "$package_name.verify" 1
+fi
 
 # extract source code and change to source directory
-if [ ! -d mpc-1.0.3 ]; then
-   tar -xf mpc-1.0.3.tar.gz
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
+else
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.start" $?
 fi
-cd mpc-1.0.3
+cd $source_dir
 
 # configure
+log_auto "$package_name.configure.start" 0
 ./configure --prefix=/usr    \
    --disable-static \
-   --docdir=/usr/share/doc/mpc-1.0.3 &&
+   --docdir=/usr/share/doc/mpc-1.0.3
+log_auto "$package_name.configure.finish" $?
 
 # build
+log_auto "$package_name.make.start" 0
 make &&
-make html &&
+make html
+log_auto "$package_name.make.finish" $?
 
 # test
-make check &&
+log_auto "$package_name.test.start" 0
+make check
+log_auto "$package_name.test.finish" $?
 
 # install
+log_auto "$package_name.install.start" 0
 make install &&
 make install-html
+log_auto "$package_name.install.finish" $?
+
+# successfully
+log_auto "$package_name.setup.finish" $?
+exit 0
