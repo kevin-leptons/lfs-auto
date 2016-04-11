e2fb014251f44386dd26c2d2a095c6a67156d5b9
kevin leptons <kevin.leptons@gmail.com>
2016-04-11 21:20:40 +0700

    add verify error for bc

--- script/build-system/build-bc.sh
+++ script/build-system/build-bc.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : build bc
+# params    : none
+# return    : 0 on success, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,31 +10,65 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# define variables
+package_name="bc"
+source_file="bc-1.06.95.tar.bz2"
+source_dir="bc-1.06.95"
+
+# log start
+log_auto "$package_name.setup.start" 0
 
 # change working directory to sources directory
-cd /sources &&
+cd /sources
+
+# verify
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
+else
+    log_auto "$package_name.verify" 1
+fi
 
 # extract source code and change to source directory
-if [ ! -d bc-1.06.95 ]; then
-   tar -xf bc-1.06.95.tar.bz2
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
+else
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.finish" $?
 fi
-cd bc-1.06.95
+cd $source_dir
 
 # path
-patch -Np1 -i ../bc-1.06.95-memory_leak-1.patch &&
+patch -Np1 -i ../bc-1.06.95-memory_leak-1.patch
+log_auto "$package_name.patch" $?
 
 # configure
+log_auto "$package_name.configure.start" 0
 ./configure --prefix=/usr           \
    --with-readline         \
    --mandir=/usr/share/man \
-   --infodir=/usr/share/info &&
+   --infodir=/usr/share/info
+log_auto "$package_name.configure.finish" $?
 
 # build
-make &&
+log_auto "$package_name.make.start" 0
+make
+log_auto "$package_name.make.finish" $?
 
 # test
-echo "quit" | ./bc/bc -l Test/checklib.b &&
+log_auto "$package_name.test.start" 0
+echo "quit" | ./bc/bc -l Test/checklib.b
+log_auto "$package_name.test.finish" $?
 
 # install
+log_auto "$package_name.install.start" 0
 make install
+log_auto "$package_name.install.finish" $?
+
+# successfully
+log_auto "$package_name.setup.finish" $?
+exit 0
