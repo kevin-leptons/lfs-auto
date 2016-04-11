8b4cd7ed47550f116a3aa21648b531526cbfc68f
kevin leptons <kevin.leptons@gmail.com>
2016-04-11 22:02:30 +0700

    add verify error for gettext

--- script/build-system/build-gettext.sh
+++ script/build-system/build-gettext.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : build gettext
+# params    : none
+# return    : 0 on success, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,27 +10,60 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# variables
+package_name="gettext"
+source_file="gettext-0.19.5.1.tar.xz"
+source_dir="gettext-0.19.5.1"
+
+# log start
+log_auto "$package_name.setup.start" 0
 
 # change working directory to sources directory
-cd /sources &&
+cd /sources
+
+# verify
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
+else
+    log_auto "$package_name.verify" 1
+fi
 
 # extract source code and change to source directory
-if [ ! -d gettext-0.19.5.1 ]; then
-   tar -xf gettext-0.19.5.1.tar.xz
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
+else
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.finish" $?
 fi
-cd gettext-0.19.5.1
+cd $source_dir
 
 # configure
+log_auto "$package_name.configure.start" 0
 ./configure --prefix=/usr    \
    --disable-static \
-   --docdir=/usr/share/doc/gettext-0.19.5.1 &&
+   --docdir=/usr/share/doc/gettext-0.19.5.1
+log_auto "$package_name.configure.finish" $?
 
 # build
-make &&
+log_auto "$package_name.make.start" 0
+make
+log_auto "$package_name.make.finish" $?
 
 # test
-make check &&
+log_auto "$package_name.test.start" 0
+make check
+log_auto "$package_name.test.finish" $?
 
 # install
+log_auto "$package_name.install.start" 0
 make install
+log_auto "$package_name.install.finish" $?
+
+# successfully
+log_auto "$package_name.setup.finish" $?
+exit 0
