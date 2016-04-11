1f17a69fc9433c94dd289d6075e8090cdf4bdde7
kevin leptons <kevin.leptons@gmail.com>
2016-04-11 22:08:33 +0700

    add verify error for groff

--- script/build-system/build-groff.sh
+++ script/build-system/build-groff.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : build groff
+# params    : none
+# return    : 0 on success, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,22 +10,53 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# variables
+package_name="groff"
+source_file="groff-1.22.3.tar.gz"
+source_dir="groff-1.22.3"
+
+# log start
+log_auto "$package_name.setup.start" 0
 
 # change working directory to sources directory
-cd /sources &&
+cd /sources
+
+# verify
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
+else
+    log_auto "$package_name.verify" 1
+fi
 
 # extract source code and change to source directory
-if [ ! -d groff-1.22.3 ]; then
-   tar -xf groff-1.22.3.tar.gz
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
+else
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.finish" $?
 fi
-cd groff-1.22.3
+cd $source_dir
 
 # configure
-PAGE=A4 ./configure --prefix=/usr &&
+log_auto "$package_name.configure.start" 0
+PAGE=A4 ./configure --prefix=/usr
+log_auto "$package_name.configure.finish" $?
 
 # build
-make &&
+log_auto "$package_name.make.start" 0
+make
+log_auto "$package_name.make.finish" $?
 
 # install
+log_auto "$package_name.install.start" 0
 make install
+log_auto "$package_name.install.finish" $?
+
+# successfully
+log_auto "$package_name.setup.finish" $?
+exit 0
