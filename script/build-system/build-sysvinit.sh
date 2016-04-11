7d721c9631be2fb31470f13d0c003da9b142db03
kevin leptons <kevin.leptons@gmail.com>
2016-04-11 23:17:11 +0700

    add verify error for build sysvinit

--- script/build-system/build-sysvinit.sh
+++ script/build-system/build-sysvinit.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : build sysvinit
+# params    : none
+# return    : 0 on success, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,23 +10,52 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# variables
+package_name="sysvinit"
+source_file="sysvinit-2.88dsf.tar.bz2"
+source_dir="sysvinit-2.88dsf"
+
+# log start
+log_auto "$package_name.setup.start" 0
 
 # change working directory to sources directory
-cd /sources &&
+cd /sources
+
+# verify
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
+else
+    log_auto "$package_name.verify" 1
+fi
 
 # extract source code and change to source directory
-if [ ! -d sysvinit-2.88dsf ]; then
-   tar -xf sysvinit-2.88dsf.tar.bz2
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
+else
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.finish" $?
 fi
-cd sysvinit-2.88dsf
+cd $source_dir
 
 # patch
-patch -Np1 -i ../sysvinit-2.88dsf-consolidated-1.patch &&
+patch -Np1 -i ../sysvinit-2.88dsf-consolidated-1.patch
+log_auto "$package_name.patch" $?
 
 # build
-make -C src &&
+log_auto "$package_name.make.start" 0
+make -C src
+log_auto "$package_name.make.finish" $?
 
 # install
+log_auto "$package_name.install.start" 0
 make -C src install
+log_auto "$package_name.install.finish" $?
 
+# successfully
+log_auto "$package_name.setup.finish" $?
+exit 0
