27169dd5ed866163cbb5c338216a6ad07e6a3de3
kevin leptons <kevin.leptons@gmail.com>
2016-04-11 21:28:29 +0700

    add verify error for expat

--- script/build-system/build-expat.sh
+++ script/build-system/build-expat.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : build expat
+# params    : none
+# return    : 0 on success, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,29 +10,63 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# define variables
+package_name="expat"
+source_file="expat-2.1.0.tar.gz"
+source_dir="expat-2.1.0"
+
+# log start
+log_auto "$package_name.setup.start" 0
 
 # change working directory to sources directory
-cd /sources &&
+cd /sources
+
+# verify
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
+else
+    log_auto "$package_name.verify" 1
+fi
 
 # extract source code and change to source directory
-if [ ! -d expat-2.1.0 ]; then
-   tar -xf expat-2.1.0.tar.gz
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
+else
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.finish" $?
 fi
-cd expat-2.1.0
+cd $source_dir
 
 # configure
-./configure --prefix=/usr --disable-static &&
+log_auto "$package_name.configure.start" 0
+./configure --prefix=/usr --disable-static
+log_auto "$package_name.configure.finish" $?
 
 # build
-make &&
+log_auto "$package_name.make.start" 0
+make
+log_auto "$package_name.make.finish" $?
 
 # test
-make check &&
+log_auto "$package_name.test.start" 0
+make check
+log_auto "$package_name.test.finish" $?
 
 # install
-make install &&
+log_auto "$package_name.install.start" 0
+make install
+log_auto "$package_name.install.finish" $?
 
 # install documents
 install -v -dm755 /usr/share/doc/expat-2.1.0 &&
 install -v -m644 doc/*.{html,png,css} /usr/share/doc/expat-2.1.0
+log_auto "$package_name.doc.install" $?
+
+# successfully
+log_auto "$package_name.setup.finish" $?
+exit 0
