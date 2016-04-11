822c517250733a1c4ede4eb4e27269301ae85de0
kevin leptons <kevin.leptons@gmail.com>
2016-04-11 18:17:23 +0700

    add verify error for build iana-etc

--- script/build-system/build-iana-etc.sh
+++ script/build-system/build-iana-etc.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : build iana-etc
+# params    : none
+# return    : 0 on success, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,19 +10,49 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# define variables
+package_name="iana-etc"
+source_file="iana-etc-2.30.tar.bz2"
+source_dir="iana-etc-2.30"
+
+# log start
+log_auto "$package_name.setup.start" 0
 
 # change working directory to sources directory
-cd /sources &&
+cd /sources
+
+# verify
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
+else
+    log_auto "$package_name.verify" 1
+fi
 
 # extract source code and change to source directory
-if [ ! -d iana-etc-2.30 ]; then
-   tar -xf iana-etc-2.30.tar.bz2
+# extract source code and change to source directory
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
+else
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.finish" $?
 fi
-cd iana-etc-2.30
+cd $source_dir
 
 # build
-make &&
+log_auto "$package_name.make.start" 0
+make
+log_auto "$package_name.make.finish" $?
 
 # install
+log_auto "$package_name.install.start" 0
 make install
+log_auto "$package_name.install.finish" $?
+
+# successfully
+log_auto "$package_name.setup.finish" $?
+exit 0
