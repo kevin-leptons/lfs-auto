1e97003a0748f12b4dfd20ecad55f4122ad7288b
kevin leptons <kevin.leptons@gmail.com>
2016-04-11 18:19:04 +0700

    add verify error for build m4

--- script/build-system/build-m4.sh
+++ script/build-system/build-m4.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : build m4
+# params    : none
+# return    : 0 on success, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,22 +10,53 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# define variables
+package_name="m4"
+source_file="m4-1.4.17.tar.xz"
+source_dir="m4-1.4.17"
+
+# log start
+log_auto "$package_name.setup.start" 0
 
 # change working directory to sources directory
-cd /sources &&
+cd /sources
+
+# verify
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
+else
+    log_auto "$package_name.verify" 1
+fi
 
 # extract source code and change to source directory
-if [ ! -d m4-1.4.17 ]; then
-   tar -xf m4-1.4.17.tar.xz
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
+else
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.finish" $?
 fi
-cd m4-1.4.17
+cd $source_dir
 
 # configure
-./configure --prefix=/usr &&
+log_auto "$package_name.configure.start" 0
+./configure --prefix=/usr
+log_auto "$package_name.configure.finish" $?
 
 # build
-make &&
+log_auto "$package_name.make.start" 0
+make
+log_auto "$package_name.make.finish" $?
 
 # install
+log_auto "$package_name.install.start" 0
 make install
+log_auto "$package_name.install.finish" $?
+
+# successfully
+log_auto "$package_name.setup.finish" $?
+exit 0
