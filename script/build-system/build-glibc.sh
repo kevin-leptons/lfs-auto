4b0be84f7c069906694425768763ac58efca25e6
kevin leptons <kevin.leptons@gmail.com>
2016-04-11 14:13:26 +0700

    add verify error for build glibc

--- script/build-system/build-glibc.sh
+++ script/build-system/build-glibc.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : build glibc
+# params    : none
+# return    : 0 on success, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,47 +10,85 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# define variables
+package_name="glibc"
+source_file="glibc-2.22.tar.xz"
+source_dir="glibc-2.22"
+build_dir="gblic-build"
+timezone_file="../tzdata2015f.tar.gz"
+
+# log start
+log_auto "$package_name.setup.start" 0
 
 # change working directory to sources directory
-cd /sources &&
+cd /sources
+
+# verify
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
+else
+    log_auto "$package_name.verify" 1
+fi
 
 # extract source code and change to source directory
-rm -rf glibc-2.22
-tar -xf glibc-2.22.tar.xz
-cd glibc-2.22 &&
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
+else
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.finish" $?
+fi
+cd $source_dir
 
 # path
+log_auto "$package_name.patch.start" 0
 patch -Np1 -i ../glibc-2.22-fhs-1.patch &&
-patch -Np1 -i ../glibc-2.22-upstream_i386_fix-1.patch &&
+patch -Np1 -i ../glibc-2.22-upstream_i386_fix-1.patch
+log_auto "$package_name.patch.finish" $?
 
 # create build directory
 cd ../
-rm -rf glibc-build
-mkdir -v glibc-build
-cd glibc-build &&
+rm -vrf $build_dir
+mkdir -vp $build_dir
+cd -v $build_dir
+log_auto "$package_name.build-dir.create" $?
 
 # configure
+log_auto "$package_name.configure.start" 0
 ../glibc-2.22/configure    \
       --prefix=/usr          \
       --disable-profile      \
       --enable-kernel=2.6.32 \
-      --enable-obsolete-rpc &&
+      --enable-obsolete-rpc
+log_auto "$package_name.configure.finish" $?
 
 # build
-make &&
+log_auto "$package_name.make.start" 0
+make
+log_auto "$package_name.make.finish" $?
 
 # test
-make check &&
+log_auto "$package_name.test.start" 0
+make check
+log_auto "$package_name.test.finish" $?
 
 # install
-make install &&
+log_auto "$package_name.install.start" 0
+make install
+log_auto "$package_name.install.finish" $?
 
 # install configuration file
+log_auto "$package_name.install-conf-file.start" 0
 cp -v ../glibc-2.22/nscd/nscd.conf /etc/nscd.conf &&
 mkdir -pv /var/cache/nscd &&
+log_auto "$package_name.install-conf-file.finish" $?
 
 # install locale
+log_auto "$package_name.install-locale.start" 0
 mkdir -pv /usr/lib/locale
 localedef -i cs_CZ -f UTF-8 cs_CZ.UTF-8
 localedef -i de_DE -f ISO-8859-1 de_DE
@@ -73,65 +113,57 @@ localedef -i tr_TR -f UTF-8 tr_TR.UTF-8
 localedef -i zh_CN -f GB18030 zh_CN.GB18030
 
 make localedata/install-locales
+log_auto "$package_name.install-locale.finish" $?
 
 # add nsswitch.conf
-cat > /etc/nsswitch.conf << "EOF"
-# Begin /etc/nsswitch.conf
+cp -vp asset/nsswitch.conf /etc/nsswitch.conf
+log_auto "/etc/nsswitch.conf.create" $?
 
-passwd: files
-group: files
-shadow: files
+# start setup timezone
+log_auto "timezone.setup.start" 0
 
-hosts: files dns
-networks: files
+# verify timezone data
+if [ -f $timezone_file ]; then
+    log_auto "$timezone_file.verify" 0
+else
+    log_auto "$timezone_file.verify" 1
+fi
 
-protocols: files
-services: files
-ethers: files
-rpc: files
+# extract timezone data
+log_auto "timezone.extract.start" 0
+tar -vxf $timezone_file
+log_auto "timezone.extract.finish" $?
 
-# End /etc/nsswitch.conf
-EOF
-
-# add time zone data
-tar -xf ../tzdata2015f.tar.gz
-
-ZONEINFO=/usr/share/zoneinfo
-mkdir -pv $ZONEINFO/{posix,right}
+# configure timezone
+ZONEINFO=/usr/share/zoneinfo &&
+mkdir -pv $ZONEINFO/{posix,right} &&
 
 for tz in etcetera southamerica northamerica europe africa antarctica  \
           asia australasia backward pacificnew systemv; do
-    zic -L /dev/null   -d $ZONEINFO       -y "sh yearistype.sh" ${tz}
-    zic -L /dev/null   -d $ZONEINFO/posix -y "sh yearistype.sh" ${tz}
+    zic -L /dev/null   -d $ZONEINFO       -y "sh yearistype.sh" ${tz} &&
+    zic -L /dev/null   -d $ZONEINFO/posix -y "sh yearistype.sh" ${tz} &&
     zic -L leapseconds -d $ZONEINFO/right -y "sh yearistype.sh" ${tz}
 done
 
-cp -v zone.tab zone1970.tab iso3166.tab $ZONEINFO
-zic -d $ZONEINFO -p America/New_York
+cp -v zone.tab zone1970.tab iso3166.tab $ZONEINFO &&
+zic -d $ZONEINFO -p Asia/Ho_Chi_Minh &&
 unset ZONEINFO
-
-# determine local time
-# select asia zone with value 5
-# select vietnam zone with value 49
-# confirm with value 1
-# name of time zone is Asia/Ho_Chi_Minh
-echo -e "5\n49\1" | tzselect
 
 # create /etc/localtime
 cp -v /usr/share/zoneinfo/Asia/Ho_Chi_Minh /etc/localtime
+log_auto "/etc/localtime.create" $?
+
+# finish setup timezone
+log_auto "timezone.setup.finish" $?
 
 # create /etc/ld.so.conf
-cat > /etc/ld.so.conf << "EOF"
-# Begin /etc/ld.so.conf
-/usr/local/lib
-/opt/lib
+cp asset/ld.so.conf /etc/ld.so.conf
+log_auto "/etc/ld.so.conf.create" $?
 
-EOF
-
-# update /etc/ld.so.conf
-cat >> /etc/ld.so.conf << "EOF"
-# Add an include directory
-include /etc/ld.so.conf.d/*.conf
-
-EOF
+# create /etc/ld.so.conf.d
 mkdir -pv /etc/ld.so.conf.d
+log_auto "/etc/ld.so.conf.d.create" $?
+
+# successfully
+log_auto "$package_name.setup.finish" $?
+exit 0
