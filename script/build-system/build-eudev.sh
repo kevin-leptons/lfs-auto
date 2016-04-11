ed6fa9be4f252f55bedaf5dad594c6f9b7f7fd05
kevin leptons <kevin.leptons@gmail.com>
2016-04-11 23:26:54 +0700

    add verify error for build eudev

--- script/build-system/build-eudev.sh
+++ script/build-system/build-eudev.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : build eudev
+# params    : none
+# return    : 0 on success, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,19 +10,41 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# variables
+package_name="eudev"
+source_file="eudev-3.1.2.tar.gz"
+source_dir="eudev-3.1.2"
+
+# log start
+log_auto "$package_name.setup.start" 0
 
 # change working directory to sources directory
-cd /sources &&
+cd /sources
+
+# verify
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
+else
+    log_auto "$package_name.verify" 1
+fi
 
 # extract source code and change to source directory
-if [ ! -d eudev-3.1.2 ]; then
-   tar -xf eudev-3.1.2.tar.gz
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
+else
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.finish" $?
 fi
-cd eudev-3.1.2
+cd $source_dir
 
 # fix a test script
-sed -r -i 's|/usr(/bin/test)|\1|' test/udev-test.pl &&
+sed -r -i 's|/usr(/bin/test)|\1|' test/udev-test.pl
+log_auto "$package_name.test-script.fix" $?
 
 # prevent the /tools
 cat > config.cache << "EOF"
@@ -28,8 +52,10 @@ HAVE_BLKID=1
 BLKID_LIBS="-lblkid"
 BLKID_CFLAGS="-I/tools/include"
 EOF
+log_auto "/tools.prevent-hard-code-into-eudev". $?
 
 # configure
+log_auto "$package_name.configure.start" 0
 ./configure --prefix=/usr           \
    --bindir=/sbin          \
    --sbindir=/sbin         \
@@ -45,21 +71,34 @@ EOF
    --disable-gudev         \
    --disable-static        \
    --config-cache          \
-   --disable-gtk-doc-html &&
+   --disable-gtk-doc-html
+log_auto "$package_name.configure.finish" $?
 
 # build
-LIBRARY_PATH=/tools/lib make &&
+log_auto "$package_name.make.start" 0
+LIBRARY_PATH=/tools/lib make
+log_auto "$package_name.make.finish" $?
 
 # create directories are needed for test
-mkdir -pv /lib/udev/rules.d
+mkdir -pv /lib/udev/rules.d &&
 mkdir -pv /etc/udev/rules.d
+log_auto "$package_name.test.prepare" $?
 
 # test
-make LD_LIBRARY_PATH=/tools/lib check &&
+log_auto "$package_name.test.start" 0
+make LD_LIBRARY_PATH=/tools/lib check
+log_auto "$package_name.test.finish" $?
 
 # install
-make LD_LIBRARY_PATH=/tools/lib install &&
+log_auto "$package_name.install.start" 0
+make LD_LIBRARY_PATH=/tools/lib install
+log_auto "$package_name.install.finish" $?
 
 # install custom rules
-tar -xvf ../udev-lfs-20140408.tar.bz2
+tar -xvf ../udev-lfs-20140408.tar.bz2 &&
 make -f udev-lfs-20140408/Makefile.lfs install
+log_auto "$package_name.custom-rule.install" $?
+
+# successfully
+log_auto "$package_name.setup.finish" $?
+exit 0
