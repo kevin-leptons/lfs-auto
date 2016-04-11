877de1d6d3d99d4fea25672536c2660f26def4c9
kevin leptons <kevin.leptons@gmail.com>
2016-04-11 22:37:17 +0700

    add verify error for build grub

--- script/build-system/build-grub.sh
+++ script/build-system/build-grub.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : build grub
+# params    : none
+# return    : 0 on success, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,27 +10,58 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# variables
+package_name="grub"
+source_file="grub-2.02~beta2.tar.xz"
+source_dir="grub-2.02~beta2"
+
+# log start
+log_auto "$package_name.setup.start" 0
 
 # change working directory to sources directory
-cd /sources &&
+cd /sources
+
+# verify
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
+else
+    log_auto "$package_name.verify" 1
+fi
 
 # extract source code and change to source directory
-if [ ! -d grub-2.02 ]; then
-   tar -xf grub-2.02~beta2.tar.xz
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
+else
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.finish" $?
 fi
-cd grub-2.02~beta2
+cd $source_dir
 
 # configure
+log_auto "$package_name.configure.start" 0
 ./configure --prefix=/usr          \
    --sbindir=/sbin        \
    --sysconfdir=/etc      \
    --disable-grub-emu-usb \
    --disable-efiemu       \
-   --disable-werror &&
+   --disable-werror
+log_auto "$package_name.configure.finish" $?
 
 # build
-make &&
+log_auto "$package_name.make.start" 0
+make
+log_auto "$package_name.make.finish" $?
 
 # install
+log_auto "$package_name.install.start" 0
 make install
+log_auto "$package_name.install.finish" $?
+
+# successfully
+log_auto "$package_name.setup.finish" $?
+exit 0
