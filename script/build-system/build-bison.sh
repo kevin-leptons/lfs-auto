e16245986a997a558e71c632da8ced00dbbd0d18
kevin leptons <kevin.leptons@gmail.com>
2016-04-11 18:24:45 +0700

    add verify error for build bison

--- script/build-system/build-bison.sh
+++ script/build-system/build-bison.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : build bison
+# params    : none
+# return    : 0 on success, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,25 +10,58 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# define variables
+package_name="bison"
+source_file="bison-3.0.4.tar.xz"
+source_dir="bison-3.0.4"
+
+# log start
+log_auto "$package_name.setup.start" 0
 
 # change working directory to sources directory
-cd /sources &&
+cd /sources
+
+# verify
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
+else
+    log_auto "$package_name.verify" 1
+fi
 
 # extract source code and change to source directory
-if [ ! -d bison-3.0.4 ]; then
-   tar -xf bison-3.0.4.tar.xz
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
+else
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.finish" $?
 fi
-cd bison-3.0.4
+cd $source_dir
 
 # configure
-./configure --prefix=/usr --docdir=/usr/share/doc/bison-3.0.4 &&
+log_auto "$package_name.configure.start"
+./configure --prefix=/usr --docdir=/usr/share/doc/bison-3.0.4
+log_auto "$package_name.configure.finish" $?
 
 # build
-make &&
+log_auto "$package_name.make.start" 0
+make
+log_auto "$package_name.make.finish" $?
 
 # test
-make check &&
+log_auto "$package_name.test.start" 0
+make check
+log_auto "$package_name.test.finish" $?
 
 # install
+log_auto "$package_name.install.start" 0
 make install
+log_auto "$package_name.install.finish" $?
+
+# successfully
+log_auto "$package_name.setup.finish" $?
+exit 0
