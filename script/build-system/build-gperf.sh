64ec034c4a1d9b73d08deb8f6d4e0805a1f2df6f
kevin leptons <kevin.leptons@gmail.com>
2016-04-11 22:06:59 +0700

    add verify error for gperf

--- script/build-system/build-gperf.sh
+++ script/build-system/build-gperf.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : build gperf
+# params    : none
+# return    : 0 on success, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,25 +10,58 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# variables
+package_name="gperf"
+source_file="gperf-3.0.4.tar.gz"
+source_dir="gperf-3.0.4"
+
+# log start
+log_auto "$package_name.setup.start" 0
 
 # change working directory to sources directory
-cd /sources &&
+cd /sources
+
+# verify
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
+else
+    log_auto "$package_name.verify" 1
+fi
 
 # extract source code and change to source directory
-if [ ! -d gperf-3.0.4 ]; then
-   tar -xf gperf-3.0.4.tar.gz
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
+else
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.finish" $?
 fi
-cd gperf-3.0.4
+cd $source_dir
 
 # configure
-./configure --prefix=/usr --docdir=/usr/share/doc/gperf-3.0.4 &&
+log_auto "$package_name.configure.start" 0
+./configure --prefix=/usr --docdir=/usr/share/doc/gperf-3.0.4
+log_auto "$package_name.configure.finish" $?
 
 # build
-make &&
+log_auto "$package_name.make.start" 0
+make
+log_auto "$package_name.make.finish" $?
 
 # test
-make -j1 check &&
+log_auto "$package_name.test.start" 0
+make -j1 check
+log_auto "$package_name.test.finish" $?
 
 # install
+log_auto "$package_name.install.start" 0
 make install
+log_auto "$package_name.install.finish" $?
+
+# successfully
+log_auto "$package_name.setup.finish" $?
+exit 0
