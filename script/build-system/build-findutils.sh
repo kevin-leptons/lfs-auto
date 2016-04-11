e35b3e40b4ba656ba956b70462c00cc97d4bac6f
kevin leptons <kevin.leptons@gmail.com>
2016-04-11 22:00:45 +0700

    add verify error for findutils

--- script/build-system/build-findutils.sh
+++ script/build-system/build-findutils.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : build findutils
+# params    : none
+# return    : 0 on success, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,29 +10,64 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# variables
+package_name="findutils"
+source_file="findutils-4.4.2.tar.gz"
+source_dir="findutils-4.4.2"
+
+# log start
+log_auto "$package_name.setup.start" 0
 
 # change working directory to sources directory
-cd /sources &&
+cd /sources
+
+# verify
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
+else
+    log_auto "$package_name.verify" 1
+fi
 
 # extract source code and change to source directory
-if [ ! -d findutils-4.4.2 ]; then
-   tar -xf findutils-4.4.2.tar.gz
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
+else
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.finish" $?
 fi
-cd findutils-4.4.2
+cd $source_dir
 
 # configure
-./configure --prefix=/usr --localstatedir=/var/lib/locate &&
+log_auto "$package_name.configure.start" 0
+./configure --prefix=/usr --localstatedir=/var/lib/locate
+log_auto "$package_name.configure.finish" $?
 
 # build
-make &&
+log_auto "$package_name.make.start" 0
+make
+log_auto "$package_name.make.finish" $?
+
 
 # test
-make check &&
+log_auto "$package_name.test.start" 0
+make check
+log_auto "$package_name.test.finish" $?
 
 # install
-make install &&
+log_auto "$package_name.install.start" 0
+make install
+log_auto "$package_name.install.finish" $?
 
 # modified correct an explicit path
-mv -v /usr/bin/find /bin
+mv -v /usr/bin/find /bin &&
 sed -i 's|find:=${BINDIR}|find:=/bin|' /usr/bin/updatedb
+log_auto "$package_name.path.modify" $?
+
+# successfully
+log_auto "$package_name.setup.finish" $?
+exit 0
