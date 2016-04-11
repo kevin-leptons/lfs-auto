1232b4ac8dd0b3db6c8dbed07070e4bf6ece0c97
kevin leptons <kevin.leptons@gmail.com>
2016-04-11 13:26:13 +0700

    add verify error for build man-pages

--- script/build-system/build-man-pages.sh
+++ script/build-system/build-man-pages.sh
@@ -8,16 +8,35 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# define variables
+package_name="man-pages"
+source_file="man-pages-4.02.tar.xz"
+source_dir="man-pages-4.02"
 
 # change working directory to sources directory
 cd /sources
 
+# log start
+log_auto "$package_name.setup.start" 0
+
 # extract source code and change to source directory
-if [ ! -d man-pages-4.02 ]; then
-   tar -xf man-pages-4.02.tar.xz
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
+else
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file 
+    log_auto "$package_name.extract.finish" $?
 fi
-cd man-pages-4.02
+cd $source_dir
 
 # install
+log_auto "$package_name.install.start" 0
 make install
+log_auto "$package_name.install.finish" $?
+
+# successfully
+exit 0
