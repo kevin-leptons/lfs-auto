6374987752a3b4548c86f0025fedbc19e3838ab8
kevin leptons <kevin.leptons@gmail.com>
2016-04-11 22:50:37 +0700

    add verify error for build gzip

--- script/build-system/build-gzip.sh
+++ script/build-system/build-gzip.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : build gzip
+# params    : none
+# return    : 0 on success, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,26 +10,63 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# variables
+package_name="gzip"
+source_file="gzip-1.6.tar.xz"
+source_dir="gzip-1.6"
+
+# log start
+log_auto "$package_name.setup.start" 0
 
 # change working directory to sources directory
-cd /sources &&
+cd /sources
+
+# verify
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
+else
+    log_auto "$package_name.verify" 1
+fi
 
 # extract source code and change to source directory
-if [ ! -d gzip-1.6.tar.xz]; then
-   tar -xf gzip-1.6.tar.xz
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
+else
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.finish" $?
 fi
-cd gzip-1.6
+cd $source_dir
 
 # configure
-./configure --prefix=/usr --bindir=/bin &&
+log_auto "$package_name.configure.start" 0
+./configure --prefix=/usr --bindir=/bin
+log_auto "$package_name.configure.finish" $?
+
+# build
+log_auto "$package_name.make.start" 0
+make
+log_auto "$package_name.make.finish" $?
 
 # test
-make check &&
+log_auto "$package_name.test.start" 0
+make check
+log_auto "$package_name.test.finish" $?
 
 # install
-make install &&
+log_auto "$package_name.install.start" 0
+make install
+log_auto "$package_name.install.finish" $?
 
 # move executable
-mv -v /bin/{gzexe,uncompress,zcmp,zdiff,zegrep} /usr/bin
+mv -v /bin/{gzexe,uncompress,zcmp,zdiff,zegrep} /usr/bin &&
 mv -v /bin/{zfgrep,zforce,zgrep,zless,zmore,znew} /usr/bin
+log_auto "$package_name.executable.move" $?
+
+# successfully
+log_auto "$package_name.setup.finish" $?
+exit 0
