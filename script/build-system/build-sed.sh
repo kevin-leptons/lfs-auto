6e5d4c77a6f616f35ad3fc0041b3e983e449b198
kevin leptons <kevin.leptons@gmail.com>
2016-04-11 16:23:23 +0700

    add verify error for build sed

--- script/build-system/build-sed.sh
+++ script/build-system/build-sed.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : build sed
+# params    : none
+# return    : 0 on success, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,27 +10,60 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# define variables
+package_name="sed"
+source_file="sed-4.2.2.tar.bz2"
+source_dir="sed-4.2.2"
+
+# log start
+log_auto "$package_name.setup.start" 0
 
 # change working directory to sources directory
-cd /sources &&
+cd /sources
+
+# verify
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
+else
+    log_auto "$package_name.verify" 1
+fi
 
 # extract source code and change to source directory
-if [ ! -d sed-4.2.2 ]; then
-   tar -xf sed-4.2.2.tar.bz2
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
+else
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.finish" $?
 fi
-cd sed-4.2.2
+cd $source_dir
 
 # configure
-./configure --prefix=/usr --bindir=/bin --htmldir=/usr/share/doc/sed-4.2.2 &&
+log_auto "$package_name.configure.start" 0
+./configure --prefix=/usr --bindir=/bin --htmldir=/usr/share/doc/sed-4.2.2
+log_auto "$package_name.configure.finish" $?
 
 # build
+log_auto "$package_name.make.start" 0
 make &&
-make html &&
+make html
+log_auto "$package_name.make.finish" $?
 
 # test
-make check &&
+log_auto "$package_name.test.start" 0
+make check
+log_auto "$package_name.test.finish" $?
 
 # install
+log_auto "$package_name.install.start" 0
 make install &&
 make -C doc install-html
+log_auto "$package_name.install.finish" $?
+
+# successfully
+log_auto "$package_name.setup.finish" $?
+exit 0
