d13e3a06642e309d82abde0ee2c52d126a4c30fa
kevin leptons <kevin.leptons@gmail.com>
2016-04-11 21:58:31 +0700

    add verify error for gawk

--- script/build-system/build-gawk.sh
+++ script/build-system/build-gawk.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : build gawk
+# params    : none
+# return    : 0 on success, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,29 +10,63 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# variables
+package_name="gawk"
+source_file="gawk-4.1.3.tar.xz"
+source_dir="gawk-4.1.3"
+
+# log start
+log_auto "$package_name.setup.start" 0
 
 # change working directory to sources directory
-cd /sources &&
+cd /sources
+
+# verify
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
+else
+    log_auto "$package_name.verify" 1
+fi
 
 # extract source code and change to source directory
-if [ ! -d gawk-4.1.3 ]; then
-   tar -xf gawk-4.1.3.tar.xz
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
+else
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.finish" $?
 fi
-cd gawk-4.1.3
+cd $source_dir
 
 # configure
-./configure --prefix=/usr &&
+log_auto "$package_name.configure.start" 0
+./configure --prefix=/usr
+log_auto "$package_name.configure.finish" $?
 
 # build
-make &&
+log_auto "$package_name.make.start" 0
+make
+log_auto "$package_name.make.finish" $?
 
 # test
-make check &&
+log_auto "$package_name.test.start" 0
+make check
+log_auto "$package_name.test.finish" $?
 
 # install
-make install &&
+log_auto "$package_name.install.start" 0
+make install
+log_auto "$package_name.install.finish" $?
 
 # install documents
-mkdir -v /usr/share/doc/gawk-4.1.3
+mkdir -v /usr/share/doc/gawk-4.1.3 &&
 cp    -v doc/{awkforai.txt,*.{eps,pdf,jpg}} /usr/share/doc/gawk-4.1.3
+log_auto "$package_name.doc.install" $?
+
+# successfully
+log_auto "$package_name.setup.finish" $?
+exit 0
