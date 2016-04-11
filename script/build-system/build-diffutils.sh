c3e9f4bb271886b1efd105417b8b3f8b087b686e
kevin leptons <kevin.leptons@gmail.com>
2016-04-11 21:56:41 +0700

    add verify error for diffutils

--- script/build-system/build-diffutils.sh
+++ script/build-system/build-diffutils.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : build diffutils
+# params    : none
+# return    : 0 on success, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,28 +10,61 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# variables
+package_name="diffutils"
+source_file="diffutils-3.3.tar.xz"
+source_dir="diffutils-3.3"
+
+# log start
+log_auto "$package_name.setup.start" 0
 
 # change working directory to sources directory
-cd /sources &&
+cd /sources
+
+# verify
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
+else
+    log_auto "$package_name.verify" 1
+fi
 
 # extract source code and change to source directory
-if [ ! -d diffutils-3.3 ]; then
-   tar -xf diffutils-3.3.tar.xz
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
+else
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.finish" $?
 fi
-cd diffutils-3.3
+cd $source_dir
 
 # fix a file so locale file are isntalled
 sed -i 's:= @mkdir_p@:= /bin/mkdir -p:' po/Makefile.in.in
+log_auto "$package_name.locale-file.fix" $?
 
 # configure
-./configure --prefix=/usr &&
+log_auto "$package_name.configure.start" 0
+./configure --prefix=/usr
+log_auto "$package_name.configure.finish" $?
 
 # build
-make &&
+log_auto "$package_name.make.start" 0
+make
+log_auto "$package_name.make.finish" $?
 
 # test
-make check &&
+log_auto "$package_name.test.start" 0
+make check
+log_auto "$package_name.test.finish" $?
 
-# install
-make insall
+log_auto "$package_name.install.start" 0
+make install
+log_auto "$package_name.install.finish" $?
+
+# successfully
+log_auto "$package_name.setup.finish" $?
+exit 0
