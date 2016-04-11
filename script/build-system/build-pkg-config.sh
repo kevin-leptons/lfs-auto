b7cad71d4090ea5d6274bc7b0ec6de2bc85a7c1a
kevin leptons <kevin.leptons@gmail.com>
2016-04-11 16:05:49 +0700

    add verify error for build pkg-config

--- script/build-system/build-pkg-config.sh
+++ script/build-system/build-pkg-config.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # using     : build pkg-config
+# params    : none
+# return    : 0 on success, 1 on error
 # author    : kevin.leptons@gmail.com
 
 # locate location of this script
@@ -8,28 +10,61 @@ __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
 # use configuration
+# use util
 source $script_dir/configuration.sh
+source $script_dir/util.sh
+
+# define variables
+package_name="pkg-config"
+source_file="pkg-config-0.28.tar.gz"
+source_dir="pkg-config-0.28"
+
+# log start
+log_auto "$package_name.setup.start" 0
 
 # change working directory to sources directory
 cd /sources &&
 
-# extract source code and change to source directory
-if [ ! -d pkg-config-0.28 ]; then
-   tar -xf pkg-config-0.28.tar.gz
+# verify
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
+else
+    log_auto "$package_name.verify" 1
 fi
-cd pkg-config-0.28
+
+# extract source code and change to source directory
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
+else
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.finish" $?
+fi
+cd $source_dir
 
 # configure
+log_auto "$package_name.configure.start" 0
 ./configure --prefix=/usr        \
    --with-internal-glib \
    --disable-host-tool  \
-   --docdir=/usr/share/doc/pkg-config-0.28 &&
+   --docdir=/usr/share/doc/pkg-config-0.28
+log_auto "$package_name.configure.finish" $?
 
 # build
-make &&
+log_auto "$package_name.make.start" 0
+make
+log_auto "$package_name.make.finish" $?
 
 # test
-make check &&
+log_auto "$package_name.test.start" 0
+make check
+log_auto "$package_name.test.finish" $?
 
 # install
+log_auto "$package_name.install.start" 0
 make install
+log_auto "$package_name.install.finish" $?
+
+# successfully
+log_auto "$package_name.setup.finish" $?
+exit 0
