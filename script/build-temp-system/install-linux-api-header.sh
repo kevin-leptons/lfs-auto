cd17e90e41107c1bf509fbfe00869fdfefa36454
kevin leptons <kevin.leptons@gmail.com>
2016-04-13 12:02:00 +0700

    fix build install-api-linux-header

--- script/build-temp-system/install-linux-api-header.sh
+++ script/build-temp-system/install-linux-api-header.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # using     : install linux api header
-# time      : 0.1 sbu
 # params    : none
 # return    : 0 on successfull, 1 on error
 # author    : kevin.leptons@gmail.com
@@ -15,70 +14,47 @@ script_dir="$(dirname $__dir__)"
 source $script_dir/configuration.sh
 source $script_dir/util.sh
 
-# change working directory to sources directory
-cd $root_sources
-
-# define variables
+# variables
 package_name=linux
 source_file=linux-4.2.tar.xz
 source_dir=linux-4.2
 
-# log start setup
-log_build "$package_name.setup.start" true
+# start
+log_auto "$package_name.setup.start" 0
 
-# verify source_file
-if [ ! -f $source_file]; then
-    log_build "$package_name.verify" false
-    exit 1
+# change working directory to sources directory
+cd $root_sources
+
+# verify
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
 else
-    log_build "$package_name.verify" true
+    log_auto "$package_name.verify" 1
 fi
 
 # extract source code and change to source code directory
-if [ ! -d $source_dir ]; then
-
-    log_build "$package_name.extract.start" true
-
-    tar -vxf $source_file
-
-    if [[ $? != 0 ]]; then
-        log_build "$package_name.extract.finish" false
-        exit 1
-    else
-        log_build "$package_name.extract.finish" true
-    fi
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
 else
-    log_build "$package_name.extract.idle" true
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_build "$package_name.extract.finish" $?
 fi
 cd $source_dir
 
 # make sure there are no table files embedded in the packaged
 make mrproper
-if [[ $? != 0 ]]; then
-    log_build "$package_name.make mrproper" false
-    exit 1
-else
-    log_build "$package_name.make mrproper" true
-fi
+log_auto "$package_name.make mrproper" $?
 
 # install
-log_build "$package_name.install.start" true
+log_auto "$package_name.install.start" 0
 make INSTALL_HDR_PATH=dest headers_install
-if [[ $? != 0 ]]; then
-    log_build "$package_name.install.finish" false
-    exit 1
-else
-    log_build "$package_name.install.finish" true
-fi
+log_auto "$package_name.install.finish" $?
 
 # copy headers
 cp -rv dest/include/* /tools/include
-if [[ $? != 0 ]]; then
-    log_build "$package_name.copy headers" false
-else
-    log_build "$package_name.copy headers" true
-fi
+log_build "$package_name.copy headers" $?
 
 # successfull
-log_build "$package_name.setup.finish" true
+log_auto "$package_name.setup.finish" $?
 exit 0
