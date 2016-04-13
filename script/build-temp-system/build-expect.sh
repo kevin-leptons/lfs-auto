9db6063489a24577f71ee8e82af20c774432f63b
kevin leptons <kevin.leptons@gmail.com>
2016-04-13 14:23:00 +0700

    fix build expect

--- script/build-temp-system/build-expect.sh
+++ script/build-temp-system/build-expect.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # using     : build expect
-# time      : 0.1 sbu
 # params    : none
 # return    : 0 on successfull, 1 on error
 # author    : kevin.leptons@gmail.com
@@ -15,95 +14,61 @@ script_dir="$(dirname $__dir__)"
 source $script_dir/configuration.sh
 source $script_dir/util.sh
 
-# define variables
-package_name=expect
-source_file=expect5.45.tar.gz
-source_dir=expect5.45
+# variables
+package_name="expect"
+source_file="expect5.45.tar.gz"
+source_dir="expect5.45"
+
+# log start setup
+log_auto "$package_name.setup.start" 0
 
 # change working directory to sources directory
 cd $root_sources
 
-# log start setup
-log_build "$package_name.setup.start" true
-
 # verify
-if [ ! -f $source_file ]; then
-    log_build "$package_name.verify" false
-    exit 1
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
 else
-    log_build "$package_name.verify" true
+    log_auto "$package_name.verify" $?
 fi
 
 # extract source code and change to source code directory
-if [ ! -d $source_dir ]; then
-
-    log_build "$package_name.extract.start" true
-
-    tar -vxf $source_file
-
-    if [[ $? != 0 ]]; then
-        log_build "$package_name.extract.finish" false
-        exit 1
-    else
-        log_build "$package_name.extract.finish" true
-    fi
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
 else
-    log_build "$package_name.extract.idle" true
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.finish" $?
 fi
 cd $source_dir
 
-# fix configure file
+# configure.fix
 cp -v configure{,.orig} &&
 sed 's:/usr/local/bin:/bin:' configure.orig > configure
-if [[ $? != 0 ]]; then
-    log_build "$package_name.fix configure file" false
-    exit 1
-else
-    log_build "$package_name.fix configure file" true
-fi
+log_auto "$package_name.fix configure file" $?
 
 # configure
-log_build "$package_name.configure.start" true
+log_auto "$package_name.configure.start" 0
 ./configure --prefix=/tools       \
    --with-tcl=/tools/lib \
    --with-tclinclude=/tools/include
-if [[ $? != 0 ]]; then
-   log_build "$package_name.configure.finish" false
-   exit 1
-else
-   log_build "$package_name.configure.finish" true
-fi
+log_auto "$package_name.configure.finish" $?
 
 # build
-log_build "$package_name.make.start" true
+log_auto "$package_name.make.start" 0
 make
-if [[ $? != 0 ]]; then
-    log_build "$package_name.make.finish" false
-    exit 1
-else
-    log_build "$package_name.make.finish" true
-fi
+log_auto "$package_name.make.finish" $?
 
 # test
-log_build "$package_name.test.start" true
+log_auto "$package_name.test.start" 0
 make test
-if [[ $? != 0 ]]; then
-    log_build "$package_name.test.finish" false
-    exit 1
-else
-    log_build "$package_name.test.finish" true
-fi
+log_auto "$package_name.test.finish" $?
 
 # install
-log_build "$package_name.install.start" true
+log_auto "$package_name.install.start" 0
 make SCRIPTS="" install
-if [[ $? != 0 ]]; then
-    log_build "$package_name.install.finish" false
-    exit 1
-else
-    log_build "$package_name.install.finish" true
-fi
+log_auto "$package_name.install.finish" $?
 
 # successfull
-log_build "$package_name.setup.finish" true
+log_auto "$package_name.setup.finish" $?
 exit 0
