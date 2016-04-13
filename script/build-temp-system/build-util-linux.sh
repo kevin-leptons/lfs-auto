1d312eda87f61cd96ccc82a7b42cf0aa61dc1ba0
kevin leptons <kevin.leptons@gmail.com>
2016-04-13 15:34:22 +0700

    fix build util-linux

--- script/build-temp-system/build-util-linux.sh
+++ script/build-temp-system/build-util-linux.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # using     : build util linux
-# time      : 0.7 sbu
 # params    : none
 # return    : 0 on successfull, 1 on error
 # author    : kevin.leptons@gmail.com
@@ -16,76 +15,52 @@ source $script_dir/configuration.sh
 source $script_dir/util.sh
 
 # define variables
-package_name=util-linux
-source_file=util-linux-2.27.tar.xz
-source_dir=util-linux-2.27
+package_name="util-linux"
+source_file="util-linux-2.27.tar.xz"
+source_dir="util-linux-2.27"
+
+# start
+log_auto "$package_name.setup.start" 0
 
 # change working directory to sources directory
 cd $root_sources
 
-# log start setup
-log_build "$package_name.setup.start" true
-
-# verify source code
-if [ ! -f $source_file ]; then
-    log_build "$package_name.verify" false
-    exit 1
+# verify
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
 else
-    log_build "$package_name.verify" true
+    log_auto "$package_name.verify" 1
 fi
 
-# extract source code and change to source code directory
-if [ ! -d $source_dir ]; then
-
-    log_build "$package_name.extract.start" true
-
-    tar -vxf $source_file
-
-    if [[ $? != 0 ]]; then
-        log_build "$package_name.extract.finish" false
-        exit 1
-    else
-        log_build "$package_name.extract.finish" true
-    fi
+# extract
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
 else
-    log_build "$package_name.extract.idle" true
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.finish" $?
 fi
 cd $source_dir
 
 # configure
-log_build "$package_name.configure.start" true
+log_auto "$package_name.configure.start" 0
 ./configure --prefix=/tools                \
             --without-python               \
             --disable-makeinstall-chown    \
             --without-systemdsystemunitdir \
             PKG_CONFIG=""
-if [[ $? != 0 ]]; then
-    log_build "$package_name.configure.finish" false
-    exit 1
-else
-    log_build "$package_name.configure.finish" true
-fi
+log_auto "$package_name.configure.finish" $?
 
 # build
-log_build "$package_name.make.start" true
+log_auto "$package_name.make.start" 0
 make
-if [[ $? != 0 ]]; then
-    log_build "$package_name.make.finish" false
-    exit 1
-else
-    log_build "$package_name.make.finish" true
-fi
+log_auto "$package_name.make.finish" $?
 
 # install
-log_build "$package_name.install.start" true
+log_auto "$package_name.install.start" 0
 make install
-if [[ $? != 0 ]]; then
-    log_build "$package_name.install.finish" false
-    exit 1
-else
-    log_build "$package_name.install.finish" true
-fi
+log_auto "$package_name.install.finish" $?
 
 # successfull
-log_build "$package_name.setup.finish" true
+log_auto "$package_name.setup.finish" 0
 exit 0
