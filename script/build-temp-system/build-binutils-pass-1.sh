c48f440e19735ee21bdfea5eeb2df6153c097a2d
kevin leptons <kevin.leptons@gmail.com>
2016-04-13 11:39:10 +0700

    fix build binutils-pass-1

--- script/build-temp-system/build-binutils-pass-1.sh
+++ script/build-temp-system/build-binutils-pass-1.sh
@@ -19,37 +19,28 @@ source $script_dir/util.sh
 cd $root_sources
 
 # define variables
-package_name=binutils-pass-1
-source_file=binutils-2.25.1.tar.bz2
-source_dir=binutils-2.25.1
-build_dir=binutils-build
+package_name="binutils-pass-1"
+source_file="binutils-2.25.1.tar.bz2"
+source_dir="binutils-2.25.1"
+build_dir="binutils-build"
 
 # log start setup
-log_build "$package_name.setup.start" true
+log_auto "$package_name.setup.start" 0
 
 # verify source code
-if [ ! -f $source_file ]; then
-    log_build "$package_name.verify" false
-    exit 1
+if [ -f $source_file ]; then
+    log_auto "$package_name.verify" 0
 else
-    log_build "$package_name.verify" true
+    log_auto "$package_name.verify" 1
 fi
 
 # extract source file
-if [ ! -d $source_dir ]; then
-
-    log_build "$package_name.extract.start" true
-
-    tar -vxf $source_file
-
-    if [[ $? != 0 ]]; then
-        log_build "$package_name.extract.finish" false
-        exit 1
-    else
-        log_build "$package_name.extract.finish" true
-    fi
+if [ -d $source_dir ]; then
+    log_auto "$package_name.extract.idle" 0
 else
-    log_build "$package_name.extract.idle" true
+    log_auto "$package_name.extract.start" 0
+    tar -vxf $source_file
+    log_auto "$package_name.extract.finish" $?
 fi
 
 # create and change to build directory
@@ -57,7 +48,7 @@ mkdir -vp $build_dir
 cd $build_dir
 
 # configure
-log_build "$package_name.configure.start" true
+log_auto "$package_name.configure.start" 0
 ../binutils-2.25.1/configure \
     --prefix=/tools \
     --with-sysroot=$LFS \
@@ -65,45 +56,27 @@ log_build "$package_name.configure.start" true
     --target=$LFS_TGT \
     --disable-nls \
     --disable-werror
-if [[ $? != 0 ]]; then
-    log_build "$package_name.configure.finish" false
-    exit 1
-else
-    log_build "$package_name.configure.finish" true
-fi
+log_auto "$package_name.configure.finish" $?
 
 # build
-log_build "$package_name.make.start" true
+log_auto "$package_name.make.start" 0
 make
-if [[ $? != 0 ]]; then
-    log_build "$package_name.make.finish" false
-    exit 1
-else
-    log_build "$package_name.make.finish" true
-fi
+log_auto "$package_name.make.finish" $?
 
 # create x64 directory
 case $(uname -m) in
     x86_64) \
 
     mkdir -v /tools/lib;
-    if $?; then
-        log_build "$package_name.create x64 directory" false
-    fi
-
-    ln -sv lib /tools/lib64;;
+    ln -sv lib /tools/lib64;
+    log_auto "$package_name.x64-directory.mkdir" $?;;
 esac
 
 # install
-log_build "$package_name.install.start" true
+log_auto "$package_name.install.start" 0
 make install
-if [[ $? != 0 ]]; then
-    log_build "$package_name.install.finish" false
-    exit 1
-else
-    log_build "$package_name.install.finish" true
-fi
+log_auto "$package_name.install.finish" $?
 
 # successfull
-log_build "$package_name.setup.finish" true
+log_auto "$package_name.setup.finish" $?
 exit 0
