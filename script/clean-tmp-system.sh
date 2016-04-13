ef89e58b421a6e33e02214714161210f9bfa0785
kevin leptons <kevin.leptons@gmail.com>
2016-04-13 11:25:12 +0700

    add clean tmp system

---
+++ script/clean-tmp-system.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# using     : clean /tools
+# warning   : run this script in lfs user
+# author    : kevin.leptons@gmail.com
+
+# locate location of this script
+__dir__="$(dirname "$0")"
+
+# use configuration
+# use util
+source configuration.sh
+source util.sh
+
+# clean installed tools
+# to avoid error when use wrong tools link from /tools
+log_auto "/tools.rm.start" 0
+sudo rm -rf /tools/*
+log_auto "/tools.rm.finish" $?
+
+# change ownership of /tools to lfs
+sudo chown lfs:lfs $root_tools
