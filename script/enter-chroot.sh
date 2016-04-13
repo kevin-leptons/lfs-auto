cf1573b9c9cf68bd9ddb35648da537521763a834
kevin leptons <kevin.leptons@gmail.com>
2016-04-14 00:18:45 +0700

    try fix enter-chroot

--- script/enter-chroot.sh
+++ script/enter-chroot.sh
@@ -15,34 +15,25 @@ task_name="chroot"
 log "$task_name.start" true
 
 # create directories onto which the file system will be mounted
-mkdir -pv $LFS/{dev,proc,sys,run}
+sudo mkdir -pv $LFS/{dev,proc,sys,run}
 log_auto "fs-directory.create" $?
 
 # create initial device nodes
-if [ -e "$LFS/dev/console" ]; then
-    log_auto "/dev/console.idle" 0
-else
-    sudo mknod -m 600 $LFS/dev/console c 5 1
-    log_auto "/dev/console.create" $?
-fi
-if [ -e "$LFS/dev/null" ]; then
-    log_auto "/dev/null.idle" 0
-else
-    sudo mknod -m 666 $LFS/dev/null c 1 3
-    log_auto "/dev/null.create" $?
-fi
+sudo mknod -m 600 $LFS/dev/console c 5 1
+sudo mknod -m 666 $LFS/dev/null c 1 3
+log_auto "dev-node.create" $?
 
 # mount and populate /dev
 sudo mount -v --bind /dev $LFS/dev
-log_auto "/dev.populate" $?
+log_auto "/dev.mount" $?
 
 # mount virtual kernel file system
-sudo mount -vt devpts devpts $LFS/dev/pts -o gid=5,mode=620 &&
-sudo mount -vt proc proc $LFS/proc &&
-sudo mount -vt sysfs sysfs $LFS/sys &&
-sudo mount -vt tmpfs tmpfs $LFS/run &&
+sudo mount -vt devpts devpts $LFS/dev/pts -o gid=5,mode=620
+sudo mount -vt proc proc $LFS/proc
+sudo mount -vt sysfs sysfs $LFS/sys
+sudo mount -vt tmpfs tmpfs $LFS/run
 if [ -h $LFS/dev/shm ]; then
-   mkdir -pv $LFS/$(readlink $LFS/dev/shm)
+   sudo mkdir -pv $LFS/$(readlink $LFS/dev/shm)
 fi
 log_auto "virtual-kernel.mount" $?
 
