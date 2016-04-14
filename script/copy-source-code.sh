372a0c96bb56699a923edd5fce7efa390ac5255e
kevin leptons <kevin.leptons@gmail.com>
2016-04-14 23:09:52 +0700

    update travis

---
+++ script/copy-source-code.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# using     : copy lfs source code from /lfs/sources
+#             require
+#              - ./prepare-partition.sh run successfully
+#              - wget-list.txt put int same directory with this script
+# author    : kevin.leptons@gmail.com
+
+# locate location of this script
+__dir__="$(dirname "$0")"
+
+# use configuration
+# use util
+source $__dir__/configuration.sh
+source $__dir__/util.sh
+
+# define variables
+task_name="lfs-source"
+
+# log start
+log_auto "$task_name.copy.start" 0
+
+# copy sources from /lfs/sources
+cp -vp /lfs/sources/* /mnt/lfs/sources
+
+# successfully
+log "$task_name.copy.finish" true
+exit 0
