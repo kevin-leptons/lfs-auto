d224bdf1f5546b8ca3181a1b3ad4d29df3e045eb
kevin leptons <kevin.leptons@gmail.com>
2016-04-14 23:10:37 +0700

    update travis

--- script/prepare.sh
+++ script/prepare.sh
@@ -9,5 +9,5 @@ __dir__="$(dirname "$0")"
 # step 1. preapre partition, where lfs build on
 $__dir__/prepare-partition.sh
 
-# step 2. download source code
-$__dir__/download-source-code.sh
+# step 2. copy source code
+$__dir__/copy-source-code.sh
