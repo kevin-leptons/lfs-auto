8b5c5cf67021b92aa5da1baf6d2df811ba77aded
kevin leptons <kevin.leptons@gmail.com>
2016-04-19 19:14:35 +0700

    update comments for build check

--- script/build-temp-system/build-check.sh
+++ script/build-temp-system/build-check.sh
@@ -9,59 +9,52 @@
 __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
-# use configuration
-# use util
+# libs
 source $script_dir/configuration.sh
 source $script_dir/util.sh
 
-# define variables
+# variables
 package_name="check"
 source_file="check-0.10.0.tar.gz"
 source_dir="check-0.10.0"
 
-# log start setup
-log "$package_name.setup.start" 0
+# step.verify
+step_verify() {
+    [ -f $source_file ]
+}
 
-# change working directory to sources directory
-cd $root_sources
-
-# verify
-if [ -f $source_file ]; then
-    log "$package_name.verify" 0
-else
-    log "$package_name.verify" 1
-fi
-
-# extract source code and change to source code directory
-if [ -d $source_dir ]; then
-    log "$package_name.extract.idle" 0
-else
-    log "$package_name.extract.start" 0
+# step.extract
+step_extract() {
     tar -vxf $source_file
-    log "$package_name.extract.finish" $?
-fi
-cd $source_dir
+}
 
-# configure
-log "$package_name.configure.start" 0
-PKG_CONFIG= ./configure -- prefix=/tools
-log "$package_name.configure.finish" $?
+# step.configure
+step_configure() {
+    PKG_CONFIG= ./configure -- prefix=/tools
+}
 
-# build
-log "$package_name.make.start" 0
-make
-log "$package_name.make.finish" $?
+# step.build
+step_build() {
+    make
+}
 
-# test
-log "$package_name.test.start" 0
-make check
-log "$package_name.test.finish" $?
+# step.test
+step_test() {
+    make test
+}
 
 # install
-log "$package_name.install.start" 0
-make install
-log "$package_name.install.finish" $?
+step_install() {
+    make install
+}
 
-# successfull
-log "$package_name.setup.finish" $?
+# run
+cd $root_tmp_sources
+run_step "$package_name.verify" step_verify
+run_step "$package_name.extract" step_extract
+cd $source_dir
+run_step "$package_name.configure" step_configure
+run_step "$package_name.build" step_build
+run_step "$package_name.test" step_test
+run_step "$package_name.install" step_install
 exit 0
