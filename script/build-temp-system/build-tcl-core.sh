bfd73ddb12f6438230b537be6d53d2e075218e19
kevin leptons <kevin.leptons@gmail.com>
2016-04-19 15:41:50 +0700

    add step index for tcl-core

--- script/build-temp-system/build-tcl-core.sh
+++ script/build-temp-system/build-tcl-core.sh
@@ -9,8 +9,7 @@
 __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
-# use configuration
-# use util
+# libs
 source $script_dir/configuration.sh
 source $script_dir/util.sh
 
@@ -19,63 +18,63 @@ package_name="tcl-core"
 source_file="tcl-core8.6.4-src.tar.gz"
 source_dir="tcl8.6.4"
 
-# log start setup
-log "$package_name.setup.start" 0
+# step.verify
+step_verify() {
+    [ -f $source_file ]
+    return $?
+}
 
-# change working directory to sources directory
-cd $root_sources
-
-# verify
-if [ -f $source_file ]; then
-    log "$package_name.verify" 0
-else
-    log "$package_name.verify" $?
-fi
-
-# extract source code and change to source directory
-if [ -d $source_dir ]; then
-    log "$package_name.extract.idle" 0
-else
-    log "$package_name.extract.start" 0
+# step.extract
+step_extract() {
     tar -vxf $source_file
-    log "$package_name.extract.finish" $?
-fi
+}
+
+# step.configure
+step_configure() {
+    cd unix
+    ./configure --prefix=/tools
+}
+
+# step.build
+step_make() {
+    make
+}
+
+# step.test
+step_test() {
+    TZ=UTC make test
+}
+
+# step.install
+step_install() {
+    make install
+}
+run_step "$package_name.install" step_install
+
+# step.libtcl.chmod
+step_libtcl_chmod() {
+    chmod -v u+x /tools/lib/libtcl8.6.so
+}
+
+# step.tcl-headers.install
+step_tcl_headers_install() {
+    make install-private-headers
+}
+
+# step.link
+step_link() {
+    ln -sv tclsh8.6 /tools/bin/tclsh
+}
+
+# run
+cd $root_sources
+run_step "$package_name.verify" step_verify
+run_step "$package_name.extract" step_extract
 cd $source_dir
-
-# configure
-log "$package_name.configure.start" 0
-cd unix
-./configure --prefix=/tools
-log "$package_name.configure.finish" $?
-
-# build
-log "$package_name.make.start" 0
-make
-log "$package_name.make.finish" $?
-
-# test
-log "$package_name.test.start" 0
-TZ=UTC make test
-log "$package_name.test.finish" $?
-
-# install
-log "$package_name.install.start" 0
-make install
-log "$package_name.install.finish" $?
-
-# make library writeable, so can removed later
-chmod -v u+x /tools/lib/libtcl8.6.so
-log "$package_name.lib.chmod" $?
-
-# install tcl headers
-log "$package_name.headers.install.start" 0
-make install-private-headers
-log "$package_name.headers.install.finish" $?
-
-# link
-ln -sv tclsh8.6 /tools/bin/tclsh
-log "$package_name.link.finish" $?
-
-# successfull
-log "$package_name.setup.finish" $?
+run_step "$package_name.configure" step_configure
+run_step "$package_name.build" step_make
+run_step "$package_name.test" step_test
+run_step "/tools/lib/libtcl8.6.so.chmod" step_libtcl_chmod
+run_step "$package_name.header.install" step_tcl_headers_install
+run_step "$package_name.link" step_link
 exit 0
