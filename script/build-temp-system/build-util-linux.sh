6cef4628293d8384cc5b9f91b853285a0190369e
kevin leptons <kevin.leptons@gmail.com>
2016-04-19 22:54:22 +0700

    add index step for texinfo, util-linux

--- script/build-temp-system/build-util-linux.sh
+++ script/build-temp-system/build-util-linux.sh
@@ -9,58 +9,51 @@
 __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
-# use configuration
-# use util
+# libs
 source $script_dir/configuration.sh
 source $script_dir/util.sh
 
-# define variables
+# variables
 package_name="util-linux"
-source_file="util-linux-2.27.tar.xz"
+source_file="../util-linux-2.27.tar.xz"
 source_dir="util-linux-2.27"
 
-# start
-log "$package_name.setup.start" 0
+# step.verify
+step_verify() {
+    [ -f $source_file ]
+}
 
-# change working directory to sources directory
-cd $root_sources
-
-# verify
-if [ -f $source_file ]; then
-    log "$package_name.verify" 0
-else
-    log "$package_name.verify" 1
-fi
-
-# extract
-if [ -d $source_dir ]; then
-    log "$package_name.extract.idle" 0
-else
-    log "$package_name.extract.start" 0
+# step.extract
+step_extract() {
     tar -vxf $source_file
-    log "$package_name.extract.finish" $?
-fi
+}
+
+# step.configure
+step_configure() {
+    ./configure --prefix=/tools                \
+                --without-python               \
+                --disable-makeinstall-chown    \
+                --without-systemdsystemunitdir \
+                PKG_CONFIG=""
+}
+
+# step.build
+step_build() {
+    make
+}
+
+# step.install
+step_install() {
+    make install
+}
+
+# run
+cd $root_tmp_sources
+run_step "$package_name.verify" step_verify
+run_step "$package_name.extract" step_extract
 cd $source_dir
+run_step "$package_name.configure" step_configure
+run_step "$package_name.build" step_build
+run_step "$package_name.install" step_install
 
-# configure
-log "$package_name.configure.start" 0
-./configure --prefix=/tools                \
-            --without-python               \
-            --disable-makeinstall-chown    \
-            --without-systemdsystemunitdir \
-            PKG_CONFIG=""
-log "$package_name.configure.finish" $?
-
-# build
-log "$package_name.make.start" 0
-make
-log "$package_name.make.finish" $?
-
-# install
-log "$package_name.install.start" 0
-make install
-log "$package_name.install.finish" $?
-
-# successfull
-log "$package_name.setup.finish" 0
 exit 0
