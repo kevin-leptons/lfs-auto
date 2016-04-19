7bfb5768db9443bb4c19425528325a168e2ae1dc
kevin leptons <kevin.leptons@gmail.com>
2016-04-19 19:44:22 +0700

    add index step for bash

--- script/build-temp-system/build-bash.sh
+++ script/build-temp-system/build-bash.sh
@@ -9,63 +9,58 @@
 __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
-# use configuration
-# use util
+# libs
 source $script_dir/configuration.sh
 source $script_dir/util.sh
 
 # variables
 package_name="bash"
-source_file="bash-4.3.30.tar.gz"
+source_file="../bash-4.3.30.tar.gz"
 source_dir="bash-4.3.30"
 
-# start
-log "$package_name.setup.start" 0
+# step.verify
+step_verify() {
+    [ -f $source_file ]
+}
 
-# change working directory to sources directory
-cd $root_sources
-
-# verify
-if [ -f $source_file ]; then
-    log "$package_name.verify" 0
-else
-    log "$package_name.verify" 1
-fi
-
-# extract source code and change to source code directory
-if [ -d $source_dir ]; then
-    log "$package_name.extract.idle" 0
-else
-    log "$package_name.extract.start" 0
+# step.extract
+step_extract() {
     tar -vxf $source_file
-    log "$package_name.extract.finish" $?
-fi
+}
+
+# step.configure
+step_configure() {
+    ./configure --prefix=/tools --without-bash-malloc
+}
+
+# step.build
+step_build() {
+    make
+}
+
+# step.test
+step_test() {
+    make tests
+}
+
+# step.install
+step_install() {
+    make install
+}
+
+# step.link
+step_link() {
+    ln -sv bash /tools/bin/s
+}
+
+# run
+cd $root_tmp_sources
+run_step "$package_name.verify" step_verify
+run_step "$package_name.extract" step_extract
 cd $source_dir
-
-# configure
-log "$package_name.configure.start" 0
-./configure --prefix=/tools --without-bash-malloc
-log "$package_name.configure.finish" $?
-
-# build
-log "$package_name.make.start" 0
-make
-log "$package_name.make.finish" $?
-
-# test
-log "$package_name.test.start" 0
-make tests
-log "$package_name.test.finish" $?
-
-# install
-log "$package_name.install.start" 0
-make install
-log "$package_name.install.finish" $?
-
-# link
-ln -sv bash /tools/bin/s
-log "$package_name.link" $?
-
-# successfull
-log "$package_name.setup.finish" $?
+run_step "$package_name.configure" step_configure
+run_step "$package_name.build" step_build
+run_step "$package_name.test" step_test
+run_step "$package_name.install" step_install
+run_step "$package_name.link" step_link
 exit 0
