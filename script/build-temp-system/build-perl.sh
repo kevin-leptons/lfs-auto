e48f9bb0b60441389ca8b250ba9332efb8fc9684
kevin leptons <kevin.leptons@gmail.com>
2016-04-19 22:32:27 +0700

    fix build perl, sed

--- script/build-temp-system/build-perl.sh
+++ script/build-temp-system/build-perl.sh
@@ -9,56 +9,48 @@
 __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
-# use configuration
-# use util
+# libs
 source $script_dir/configuration.sh
 source $script_dir/util.sh
 
-# define variables
+# variables
 package_name="perl"
-source_file="perl-5.22.0.tar.bz2"
+source_file="../perl-5.22.0.tar.bz2"
 source_dir="perl-5.22.0"
 
-# start
-log "$package_name.setup.start" 0
+# test.verify
+step_verify() {
+    [ -f $source_file ]
+}
 
-# change working directory to sources directory
-cd $root_sources
-
-# verify
-if [ -f $source_file ]; then
-    log "$package_name.verify" 0
-else
-    log "$package_name.verify" 1
-fi
-
-# extract
-if [ -d $source_dir ]; then
-    log "$package_name.extract.idle" 0
-else
-    log "$package_name.extract.start" 0
+# step.extract
+step_extract() {
     tar -vxf $source_file
-    log "$package_name.extract.finish" $?
-fi
+}
+
+# step.configure
+step_configure() {
+    sh Configure -des -Dprefix=/tools -Dlibs=-lm
+}
+
+# step.build
+step_build() {
+    make
+}
+
+# step.install
+step_install() {
+    cp -v perl cpan/podlators/pod2man /tools/bin &&
+    mkdir -pv /tools/lib/perl5/5.22.0 &&
+    cp -Rv lib/* /tools/lib/perl5/5.22.0
+}
+
+# run
+cd $root_tmp_sources
+run_step "$package_name.verify" step_verify
+run_step "$package_name.extract" step_extract
 cd $source_dir
-
-# configure
-log "$package_name.configure.start" 0
-sh Configure -des -Dprefix=/tools -Dlibs=-lm
-log "$package_name.configure.finish" $?
-
-# build
-log "$package_name.make.start" 0
-make
-log "$package_name.make.finish" $?
-
-# install
-log "$package_name.install.start" 0
-cp -v perl cpan/podlators/pod2man /tools/bin &&
-mkdir -pv /tools/lib/perl5/5.22.0 &&
-cp -Rv lib/* /tools/lib/perl5/5.22.0
-log "$package_name.install.finish" $?
-
-# successfull
-log "$package_name.setup.finish" 0
+run_step "$package_name.configure" step_configure
+run_step "$package_name.build" step_build
+run_step "$package_name.install"
 exit 0
