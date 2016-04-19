f73fc4b9b6b0361259a9090fe6d6c0f6173b2faa
kevin leptons <kevin.leptons@gmail.com>
2016-04-19 19:54:31 +0700

    add index step for bzip2

--- script/build-temp-system/build-bzip2.sh
+++ script/build-temp-system/build-bzip2.sh
@@ -9,49 +9,40 @@
 __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
-# use configuration
-# use util
+# libs
 source $script_dir/configuration.sh
 source $script_dir/util.sh
 
-# define variables
+# variables
 package_name="bzip2"
-source_file="bzip2-1.0.6.tar.gz"
+source_file="../bzip2-1.0.6.tar.gz"
 source_dir="bzip2-1.0.6"
 
-# start
-log "$package_name.setup.start" 0
+# step.verify
+step_verify() {
+    [ -f $source_file ]
+}
 
-# change working directory to sources directory
-cd $root_sources
-
-# verify
-if [ -f $source_file ]; then
-    log "$package_name.verify" 0
-else
-    log "$package_name.verify" 1
-fi
-
-# extract source code and change to source code directory
-if [ -d $source_dir ]; then
-    log "$package_name.extract.idle" 0
-else
-    log "$package_name.extract.start" 0
+# step.extract
+step_extract() {
     tar -vxf $source_file
-    log "$package_name.extract.finish" $?
-fi
+}
+
+# step.build
+step_build() {
+    make
+}
+
+# step.install
+step_install() {
+    make PREFIX=/tools install
+}
+
+# run
+cd $root_tmp_sources
+run_step "$package_name.verify" step_verify
+run_step "$package_name.extract" step_extract
 cd $source_dir
-
-# build
-log "$package_name.make.start" 0
-make
-log "$package_name.make.finish" $?
-
-# install
-log "$package_name.install.start" 0
-make PREFIX=/tools install
-log "$package_name.install.finish" $?
-
-# successfull
-log "$package_name.setup.finish" $?
+run_step "$package_name.make" step_build
+run_step "$package_name.install" step_install
 exit 0
