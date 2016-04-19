301c29937c4d90e46c4329c8436ef02410b7dc9c
kevin leptons <kevin.leptons@gmail.com>
2016-04-19 19:16:35 +0700

    fix index step for build dejagnu

--- script/build-temp-system/build-dejagnu.sh
+++ script/build-temp-system/build-dejagnu.sh
@@ -15,7 +15,7 @@ source $script_dir/util.sh
 
 # variables
 package_name="dejagnu"
-source_file="dejagnu-1.5.3.tar.gz"
+source_file="../dejagnu-1.5.3.tar.gz"
 source_dir="dejagnu-1.5.3"
 
 # step.verify
