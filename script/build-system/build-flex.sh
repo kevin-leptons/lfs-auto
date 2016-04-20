3952091d43c5540b0c065ec20f0c98e5472f7949
kevin leptons <kevin.leptons@gmail.com>
2016-04-20 16:42:02 +0700

    add index step for sys

--- script/build-system/build-flex.sh
+++ script/build-system/build-flex.sh
@@ -9,67 +9,64 @@
 __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
-# use configuration
-# use util
+# libs
 source $script_dir/configuration.sh
 source $script_dir/util.sh
 
-# define variables
-package_name="flex"
-source_file="flex-2.5.39.tar.xz"
+# variables
+package_name="sys.flex"
+source_file="../flex-2.5.39.tar.xz"
 source_dir="flex-2.5.39"
 
-# log start
-log "$package_name.setup.start" 0
+# step.verify
+step_verify() {
+    [ -f $source_file ]
+}
 
-# change working directory to sources directory
-cd /sources
-
-# verify
-if [ -f $source_file ]; then
-    log "$package_name.verify" 0
-else
-    log "$package_name.verify" 1
-fi
-
-# extract source code and change to source directory
-if [ -d $source_dir ]; then
-    log "$package_name.extract.idle" 0
-else
-    log "$package_name.extract.start" 0
+# step.extract
+step_extract() {
     tar -vxf $source_file
-    log "$package_name.extract.finish" $?
-fi
+}
+
+# step.regression-tests.skip
+step_regression_test_skip() {
+    sed -i -e '/test-bison/d' tests/Makefile.in
+}
+
+# step.configure
+step_configure() {
+    ./configure --prefix=/usr --docdir=/usr/share/doc/flex-2.5.39
+}
+
+# step.build
+step_build() {
+    make
+}
+
+# step.test
+step_test() {
+    make check
+}
+
+# step.install
+step_install() {
+    make install
+}
+
+# step.link
+step_link() {
+    ln -sv flex /usr/bin/lex
+}
+
+# run
+cd $root_system_sources
+run_step "$package_name" step_verify
+run_step "$package_name.extract" step_extract
 cd $source_dir
-
-# skip three regression tests that require bison
-sed -i -e '/test-bison/d' tests/Makefile.in
-log "$package_name.regression-tests.skip" $?
-
-# configure
-log "$package_name.configure.start" 0
-./configure --prefix=/usr --docdir=/usr/share/doc/flex-2.5.39
-log "$package_name.configure.finish" $?
-
-# build
-log "$package_name.make.start" 0
-make
-log "$package_name.make.finish" $?
-
-# test
-log "$package_name.test.start" 0
-make check
-log "$package_name.test.finish" $?
-
-# install
-log "$package_name.install.start" 0
-make install
-log "$package_name.install.finish" $?
-
-# link
-ln -sv flex /usr/bin/lex
-log "$package_name.executable.link" $?
-
-# successfully
-log "$package_name.setup.finish" $?
+run_step "$package_name.regression-tests.skip" step_regression_test_skip
+run_step "$package_name.configure" step_configure
+run_step "$package_name.build" step_build
+run_step "$package_name.test" step_test
+run_step "$package_name.install" step_install
+run_step "$package_name.link" step_link
 exit 0
--- script/build-system/build-iana-etc.sh
+++ script/build-system/build-iana-etc.sh
@@ -9,50 +9,41 @@
 __dir__="$(dirname "$0")"
 script_dir="$(dirname $__dir__)"
 
-# use configuration
-# use util
+# libs
 source $script_dir/configuration.sh
 source $script_dir/util.sh
 
-# define variables
-package_name="iana-etc"
-source_file="iana-etc-2.30.tar.bz2"
+# variables
+package_name="sys.iana-etc"
+source_file="../iana-etc-2.30.tar.bz2"
 source_dir="iana-etc-2.30"
 
-# log start
-log "$package_name.setup.start" 0
+# step.verify
+step_verify() {
+    [ -f $source_file ]
+}
 
-# change working directory to sources directory
-cd /sources
-
-# verify
-if [ -f $source_file ]; then
-    log "$package_name.verify" 0
-else
-    log "$package_name.verify" 1
-fi
-
-# extract source code and change to source directory
-# extract source code and change to source directory
-if [ -d $source_dir ]; then
-    log "$package_name.extract.idle" 0
-else
-    log "$package_name.extract.start" 0
+# step.extract
+step_extract() {
     tar -vxf $source_file
-    log "$package_name.extract.finish" $?
-fi
+}
+
+# step.build
+step_build() {
+    make
+}
+
+# step.install
+step_install() {
+    make install
+}
+
+# run
+cd $root_system_sources
+run_step "$package_name.verify" step_verify
+run_step "$package_name.extract" step_extract
 cd $source_dir
+run_step "$package_name.build" step_build
+run_step "$package_name.install" step_install
 
-# build
-log "$package_name.make.start" 0
-make
-log "$package_name.make.finish" $?
-
-# install
-log "$package_name.install.start" 0
-make install
-log "$package_name.install.finish" $?
-
-# successfully
-log "$package_name.setup.finish" $?
 exit 0
