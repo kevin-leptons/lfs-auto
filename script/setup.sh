ed496c8896875ec9c04520e29bf1d46b3abfa77c
kevin leptons <kevin.leptons@gmail.com>
2016-04-23 19:00:01 +0700

    re construct project

--- script/setup.sh
+++ script/setup.sh
@@ -6,19 +6,19 @@
 # use util
 source util.sh
 
-# step 3. copy sources
+# put sources code into correct directory
 ./source-code.copy.sh
 exit_on_error
 
-# step 4. build programm as tools what use to build when enter lfs root
+# setup package of temporary system
 ./tmp-sys.pack.setup.sh
 exit_on_error
 
-# active tmp-sys
-# inside of tmp-sys, continue setup sys
+# active temporary system
+# inside of tmp-sys, continue setup system
 ./tmp-sys.active.sh
 exit_on_error
 
-# enter sys
-# continue sys.inside.setup
+# enter system
+# continue setup system inside
 ./sys.active.sh
